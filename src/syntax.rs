6c3cba9a8618294487001310ed2ed7fb0df4fad2
WATANABE Yuki <magicant@wonderwand.net>
2020-12-06 23:52:18 +0900

    Define RedirBody and Redir

--- src/syntax.rs
+++ src/syntax.rs
@@ -20,6 +20,7 @@
 
 use itertools::Itertools;
 use std::fmt;
+use std::os::unix::io::RawFd;
 
 /// Token that may involve expansion.
 ///
@@ -28,9 +29,90 @@ use std::fmt;
 #[derive(Debug)]
 pub struct Word(pub String); // TODO Redefine as a vector of word elements.
 
+impl Word {
+    /// Creates a constant word with unknown source.
+    ///
+    /// This is a convenience function to make a simple word, mainly for debugging
+    /// purpose.
+    ///
+    /// The resulting word elements are not quoted even if they would be usually special.
+    pub fn with_str(s: &str) -> Word {
+        Word(s.to_string())
+    }
+}
+
 impl fmt::Display for Word {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0)
+    }
+}
+
+/// Part of a redirection that defines the nature of the resulting file descriptor.
+pub enum RedirBody {
+    // TODO filename-based redirections
+    /// Here-document.
+    HereDoc {
+        /// Token that marks the end of the content of the here-document.
+        delimiter: Word,
+
+        /// Whether leading tab characters should be removed from each line of the
+        /// here-document content. This value is `true` for the `<<-` operator and
+        /// `false` for `<<`.
+        remove_tabs: bool,
+
+        /// Content of the here-document.
+        ///
+        /// The content ends with a newline unless it is empty. If the delimiter
+        /// is quoted, the content must not contain any expansion.
+        content: Word,
+    },
+}
+
+impl fmt::Display for RedirBody {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RedirBody::HereDoc {
+                delimiter,
+                remove_tabs,
+                ..
+            } => {
+                f.write_str(if *remove_tabs { "<<-" } else { "<<" })?;
+                write!(f, "{}", *delimiter)
+            }
+        }
+    }
+}
+
+/// Redirection.
+pub struct Redir {
+    /// File descriptor that is modified by this redirection.
+    pub fd: Option<RawFd>,
+    /// Nature of the resulting file descriptor.
+    pub body: RedirBody,
+}
+
+// TODO Should be somewhere else.
+const STDIN_FD: RawFd = 0;
+// const STDOUT_FD: RawFd = 1;
+
+impl Redir {
+    /// Computes the file descriptor that is modified by this redirection.
+    ///
+    /// If `self.fd` is `Some(_)`, the `RawFd` value is returned intact. Otherwise,
+    /// the default file descriptor is selected depending on the type of `self.body`.
+    pub fn fd_or_default(&self) -> RawFd {
+        self.fd.unwrap_or_else(|| match self.body {
+            RedirBody::HereDoc { .. } => STDIN_FD,
+        })
+    }
+}
+
+impl fmt::Display for Redir {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(fd) = self.fd {
+            write!(f, "{}", fd)?;
+        }
+        write!(f, "{}", self.body)
     }
 }
 
@@ -50,5 +132,52 @@ impl fmt::Display for SimpleCommand {
             return Ok(());
         }
         write!(f, "{}", self.words.iter().format(" "))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn redir_body_here_doc_display() {
+        let heredoc = RedirBody::HereDoc {
+            delimiter: Word::with_str("END"),
+            remove_tabs: true,
+            content: Word::with_str("here"),
+        };
+        assert_eq!(format!("{}", heredoc), "<<-END");
+
+        let heredoc = RedirBody::HereDoc {
+            delimiter: Word::with_str("XXX"),
+            remove_tabs: false,
+            content: Word::with_str("there"),
+        };
+        assert_eq!(format!("{}", heredoc), "<<XXX");
+    }
+
+    #[test]
+    fn redir_display() {
+        let heredoc = RedirBody::HereDoc {
+            delimiter: Word::with_str("END"),
+            remove_tabs: false,
+            content: Word::with_str("here"),
+        };
+
+        let redir = Redir {
+            fd: None,
+            body: heredoc,
+        };
+        assert_eq!(format!("{}", redir), "<<END");
+        let redir = Redir {
+            fd: Some(0),
+            ..redir
+        };
+        assert_eq!(format!("{}", redir), "0<<END");
+        let redir = Redir {
+            fd: Some(9),
+            ..redir
+        };
+        assert_eq!(format!("{}", redir), "9<<END");
     }
 }
