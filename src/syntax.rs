a9c9264135e7ea5872704b0a69710c0f0dfb5c11
WATANABE Yuki <magicant@wonderwand.net>
2020-12-06 21:33:30 +0900

    Define simple command

--- src/syntax.rs
+++ src/syntax.rs
@@ -14,8 +14,11 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+//! Shell command language syntax.
+//!
 //! TODO Elaborate
 
+use itertools::Itertools;
 use std::fmt;
 
 // TODO Support full syntax
@@ -27,5 +30,37 @@ pub struct Command {
 impl fmt::Display for Command {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "{}", self.content)
+    }
+}
+
+/// Token that may involve expansion.
+///
+/// It depends on context whether an empty word is valid or not. It is your responsibility to
+/// ensure a word is non-empty in a context where it cannot.
+#[derive(Debug)]
+pub struct Word(pub String); // TODO Redefine as a vector of word elements.
+
+impl fmt::Display for Word {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+/// Command that involves assignments, redirections, and word expansions.
+///
+/// In the shell language syntax, a valid simple command must contain at least one of assignments,
+/// redirections, and words. The parser must not produce a completely empty simple command.
+#[derive(Debug)]
+pub struct SimpleCommand {
+    pub words: Vec<Word>,
+    // TODO Assignments and redirections
+}
+
+impl fmt::Display for SimpleCommand {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.words.is_empty() {
+            return Ok(());
+        }
+        write!(f, "{}", self.words.iter().format(" "))
     }
 }
