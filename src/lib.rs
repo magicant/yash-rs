1756508b9e7ab5689f8bde150abe44a875dba310
WATANABE Yuki <magicant@wonderwand.net>
2020-12-15 00:29:27 +0900

    Remove test sample

--- src/lib.rs
+++ src/lib.rs
@@ -49,11 +49,3 @@ pub fn bin_main() {
         .expect("spawn should succeed");
     pool.run();
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
