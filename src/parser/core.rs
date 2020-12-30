398a277ffc5248d393e48e24cadb29950052d9fa
WATANABE Yuki <magicant@wonderwand.net>
2020-12-30 16:14:51 +0900

    Remove AsyncFnOnce

--- src/parser/core.rs
+++ src/parser/core.rs
@@ -163,26 +163,6 @@ impl<T> Rec<T> {
 /// Dummy trait for working around type error.
 ///
 /// cf. https://stackoverflow.com/a/64325742
-pub trait AsyncFnOnce<'a, T, R> {
-    type Output: Future<Output = R>;
-    fn call(self, t: &'a mut T) -> Self::Output;
-}
-
-impl<'a, T, F, R, Fut> AsyncFnOnce<'a, T, R> for F
-where
-    T: 'a,
-    F: FnOnce(&'a mut T) -> Fut,
-    Fut: Future<Output = R>,
-{
-    type Output = Fut;
-    fn call(self, t: &'a mut T) -> Fut {
-        self(t)
-    }
-}
-
-/// Dummy trait for working around type error.
-///
-/// cf. https://stackoverflow.com/a/64325742
 pub trait AsyncFnMut<'a, T, R> {
     type Output: Future<Output = R>;
     fn call(&mut self, t: &'a mut T) -> Self::Output;
