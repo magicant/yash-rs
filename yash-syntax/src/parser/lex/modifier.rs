9a28e6a798a0cc2f7e1768dd0bc63e1b78ddc3de
WATANABE Yuki <magicant@wonderwand.net>
2021-06-06 00:35:29 +0900

    Parse trim

--- yash-syntax/src/parser/lex/modifier.rs
+++ yash-syntax/src/parser/lex/modifier.rs
@@ -26,45 +26,74 @@ use crate::syntax::Modifier;
 use crate::syntax::Switch;
 use crate::syntax::SwitchCondition;
 use crate::syntax::SwitchType;
+use crate::syntax::Trim;
+use crate::syntax::TrimLength;
+use crate::syntax::TrimSide;
 use crate::syntax::Word;
 use std::future::Future;
 use std::pin::Pin;
 
 impl Lexer {
+    async fn invalid_modifier(&mut self) -> Result<Modifier> {
+        let cause = SyntaxError::InvalidModifier.into();
+        let location = self.location().await?.clone();
+        Err(Error { cause, location })
+    }
+
     async fn suffix_modifier_not_found(&mut self, colon: bool) -> Result<Modifier> {
         if colon {
-            let cause = SyntaxError::InvalidModifier.into();
-            let location = self.location().await?.clone();
-            Err(Error { cause, location })
+            self.invalid_modifier().await
         } else {
             Ok(Modifier::None)
         }
     }
+
+    /// Parses a [trim](Trim).
+    ///
+    /// This function blindly consumes the current character, which must be
+    /// `symbol`.
+    async fn trim(&mut self, colon: bool, symbol: char) -> Result<Modifier> {
+        if colon {
+            return self.invalid_modifier().await;
+        }
+
+        self.consume_char();
+        let side = match symbol {
+            '#' => TrimSide::Prefix,
+            '%' => TrimSide::Suffix,
+            _ => unreachable!(),
+        };
+
+        let length = if self.skip_if(|c| c == symbol).await? {
+            TrimLength::Longest
+        } else {
+            TrimLength::Shortest
+        };
+
+        let mut lexer = WordLexer {
+            lexer: self,
+            context: WordContext::Word,
+        };
+        // Boxing needed for recursion
+        let pattern =
+            Box::pin(lexer.word(|c| c == '}')) as Pin<Box<dyn Future<Output = Result<Word>>>>;
+        let mut pattern = pattern.await?;
+        pattern.parse_tilde_front();
+
+        Ok(Modifier::Trim(Trim {
+            side,
+            length,
+            pattern,
+        }))
+    }
 }
 
 impl WordLexer<'_> {
-    /// Parses a suffix modifier, i.e., a modifier other than the length prefix.
+    /// Parses a [switch](Switch), except the optional initial colon.
     ///
-    /// If there is a [switch](Switch), [`self.context`](Self::context) affects
-    /// how the word of the switch is parsed: If the context is `Word`, a tilde
-    /// expansion is recognized at the beginning of the word and any character
-    /// can be escaped by a backslash. If the context is `Text`, only `$`, `"`,
-    /// `` ` ``, `\` and `}` can be escaped and single quotes are not recognized
-    /// in the word.
-    pub async fn suffix_modifier(&mut self) -> Result<Modifier> {
-        let colon = self.skip_if(|c| c == ':').await?;
-
-        let r#type = if let Some(c) = self.peek_char().await? {
-            match c.value {
-                '+' => SwitchType::Alter,
-                '-' => SwitchType::Default,
-                '=' => SwitchType::Assign,
-                '?' => SwitchType::Error,
-                _ => return self.suffix_modifier_not_found(colon).await,
-            }
-        } else {
-            return self.suffix_modifier_not_found(colon).await;
-        };
+    /// This function blindly consumes the current character, which must
+    /// correspond to the `type` parameter.
+    async fn switch(&mut self, colon: bool, r#type: SwitchType) -> Result<Modifier> {
         self.consume_char();
 
         let condition = if colon {
@@ -88,6 +117,32 @@ impl WordLexer<'_> {
         };
         Ok(Modifier::Switch(switch))
     }
+
+    /// Parses a suffix modifier, i.e., a modifier other than the length prefix.
+    ///
+    /// If there is a [switch](Switch), [`self.context`](Self::context) affects
+    /// how the word of the switch is parsed: If the context is `Word`, a tilde
+    /// expansion is recognized at the beginning of the word and any character
+    /// can be escaped by a backslash. If the context is `Text`, only `$`, `"`,
+    /// `` ` ``, `\` and `}` can be escaped and single quotes are not recognized
+    /// in the word.
+    pub async fn suffix_modifier(&mut self) -> Result<Modifier> {
+        let colon = self.skip_if(|c| c == ':').await?;
+
+        if let Some(c) = self.peek_char().await? {
+            let symbol = c.value;
+            match symbol {
+                '+' => self.switch(colon, SwitchType::Alter).await,
+                '-' => self.switch(colon, SwitchType::Default).await,
+                '=' => self.switch(colon, SwitchType::Assign).await,
+                '?' => self.switch(colon, SwitchType::Error).await,
+                '#' | '%' => self.trim(colon, symbol).await,
+                _ => self.suffix_modifier_not_found(colon).await,
+            }
+        } else {
+            self.suffix_modifier_not_found(colon).await
+        }
+    }
 }
 
 #[cfg(test)]
@@ -95,6 +150,7 @@ mod tests {
     use super::*;
     use crate::parser::core::ErrorCause;
     use crate::source::Source;
+    use crate::syntax::Text;
     use crate::syntax::TextUnit;
     use crate::syntax::WordUnit;
     use futures::executor::block_on;
@@ -388,6 +444,143 @@ mod tests {
     }
 
     #[test]
+    fn lexer_suffix_modifier_trim_shortest_prefix_in_word_context() {
+        let mut lexer = Lexer::with_source(Source::Unknown, "#'*'}");
+        let mut lexer = WordLexer {
+            lexer: &mut lexer,
+            context: WordContext::Word,
+        };
+
+        let result = block_on(lexer.suffix_modifier()).unwrap();
+        if let Modifier::Trim(trim) = result {
+            assert_eq!(trim.side, TrimSide::Prefix);
+            assert_eq!(trim.length, TrimLength::Shortest);
+            assert_eq!(trim.pattern.units, [WordUnit::SingleQuote("*".to_string())]);
+            assert_eq!(trim.pattern.location.line.value, "#'*'}");
+            assert_eq!(trim.pattern.location.column.get(), 2);
+        } else {
+            panic!("Not a trim: {:?}", result);
+        }
+
+        assert_eq!(block_on(lexer.peek_char()).unwrap().unwrap().value, '}');
+    }
+
+    #[test]
+    fn lexer_suffix_modifier_trim_shortest_prefix_in_text_context() {
+        let mut lexer = Lexer::with_source(Source::Unknown, "#'*'}");
+        let mut lexer = WordLexer {
+            lexer: &mut lexer,
+            context: WordContext::Text,
+        };
+
+        let result = block_on(lexer.suffix_modifier()).unwrap();
+        if let Modifier::Trim(trim) = result {
+            assert_eq!(trim.side, TrimSide::Prefix);
+            assert_eq!(trim.length, TrimLength::Shortest);
+            assert_eq!(trim.pattern.units, [WordUnit::SingleQuote("*".to_string())]);
+            assert_eq!(trim.pattern.location.line.value, "#'*'}");
+            assert_eq!(trim.pattern.location.column.get(), 2);
+        } else {
+            panic!("Not a trim: {:?}", result);
+        }
+
+        assert_eq!(block_on(lexer.peek_char()).unwrap().unwrap().value, '}');
+    }
+
+    #[test]
+    fn lexer_suffix_modifier_trim_longest_prefix() {
+        let mut lexer = Lexer::with_source(Source::Unknown, r#"##"?"}"#);
+        let mut lexer = WordLexer {
+            lexer: &mut lexer,
+            context: WordContext::Word,
+        };
+
+        let result = block_on(lexer.suffix_modifier()).unwrap();
+        if let Modifier::Trim(trim) = result {
+            assert_eq!(trim.side, TrimSide::Prefix);
+            assert_eq!(trim.length, TrimLength::Longest);
+            assert_eq!(trim.pattern.units.len(), 1, "{:?}", trim.pattern);
+            if let WordUnit::DoubleQuote(Text(units)) = &trim.pattern.units[0] {
+                assert_eq!(units[..], [TextUnit::Literal('?')]);
+            } else {
+                panic!("Not a double quote: {:?}", trim.pattern);
+            }
+            assert_eq!(trim.pattern.location.line.value, r#"##"?"}"#);
+            assert_eq!(trim.pattern.location.column.get(), 3);
+        } else {
+            panic!("Not a trim: {:?}", result);
+        }
+
+        assert_eq!(block_on(lexer.peek_char()).unwrap().unwrap().value, '}');
+    }
+
+    #[test]
+    fn lexer_suffix_modifier_trim_shortest_suffix() {
+        let mut lexer = Lexer::with_source(Source::Unknown, r"%\%}");
+        let mut lexer = WordLexer {
+            lexer: &mut lexer,
+            context: WordContext::Word,
+        };
+
+        let result = block_on(lexer.suffix_modifier()).unwrap();
+        if let Modifier::Trim(trim) = result {
+            assert_eq!(trim.side, TrimSide::Suffix);
+            assert_eq!(trim.length, TrimLength::Shortest);
+            assert_eq!(
+                trim.pattern.units,
+                [WordUnit::Unquoted(TextUnit::Backslashed('%'))]
+            );
+            assert_eq!(trim.pattern.location.line.value, r"%\%}");
+            assert_eq!(trim.pattern.location.column.get(), 2);
+        } else {
+            panic!("Not a trim: {:?}", result);
+        }
+
+        assert_eq!(block_on(lexer.peek_char()).unwrap().unwrap().value, '}');
+    }
+
+    #[test]
+    fn lexer_suffix_modifier_trim_longest_suffix() {
+        let mut lexer = Lexer::with_source(Source::Unknown, "%%%}");
+        let mut lexer = WordLexer {
+            lexer: &mut lexer,
+            context: WordContext::Word,
+        };
+
+        let result = block_on(lexer.suffix_modifier()).unwrap();
+        if let Modifier::Trim(trim) = result {
+            assert_eq!(trim.side, TrimSide::Suffix);
+            assert_eq!(trim.length, TrimLength::Longest);
+            assert_eq!(
+                trim.pattern.units,
+                [WordUnit::Unquoted(TextUnit::Literal('%'))]
+            );
+            assert_eq!(trim.pattern.location.line.value, "%%%}");
+            assert_eq!(trim.pattern.location.column.get(), 3);
+        } else {
+            panic!("Not a trim: {:?}", result);
+        }
+
+        assert_eq!(block_on(lexer.peek_char()).unwrap().unwrap().value, '}');
+    }
+
+    #[test]
+    fn lexer_suffix_modifier_tilde_expansion_in_trim_word() {
+        let mut lexer = Lexer::with_source(Source::Unknown, r"#~}");
+        let mut lexer = WordLexer {
+            lexer: &mut lexer,
+            context: WordContext::Word,
+        };
+
+        let result = block_on(lexer.suffix_modifier()).unwrap();
+        if let Modifier::Trim(trim) = result {
+            assert_eq!(trim.pattern.units, [WordUnit::Tilde("".to_string())]);
+        } else {
+            panic!("Not a trim: {:?}", result);
+        }
+    }
+
+    #[test]
     fn lexer_suffix_modifier_orphan_colon_eof() {
         let mut lexer = Lexer::with_source(Source::Unknown, r":");
         let mut lexer = WordLexer {
@@ -402,7 +595,7 @@ mod tests {
     }
 
     #[test]
-    fn lexer_suffix_modifier_orphan_colon() {
+    fn lexer_suffix_modifier_orphan_colon_followed_by_letter() {
         let mut lexer = Lexer::with_source(Source::Unknown, r":x}");
         let mut lexer = WordLexer {
             lexer: &mut lexer,
@@ -412,6 +605,20 @@ mod tests {
         let e = block_on(lexer.suffix_modifier()).unwrap_err();
         assert_eq!(e.cause, ErrorCause::Syntax(SyntaxError::InvalidModifier));
         assert_eq!(e.location.line.value, ":x}");
+        assert_eq!(e.location.column.get(), 2);
+    }
+
+    #[test]
+    fn lexer_suffix_modifier_orphan_colon_followed_by_symbol() {
+        let mut lexer = Lexer::with_source(Source::Unknown, r":#}");
+        let mut lexer = WordLexer {
+            lexer: &mut lexer,
+            context: WordContext::Word,
+        };
+
+        let e = block_on(lexer.suffix_modifier()).unwrap_err();
+        assert_eq!(e.cause, ErrorCause::Syntax(SyntaxError::InvalidModifier));
+        assert_eq!(e.location.line.value, ":#}");
         assert_eq!(e.location.column.get(), 2);
     }
 }
