f4a27fff851e233aa21019d80a9f81478d1f2f9c
WATANABE Yuki <magicant@wonderwand.net>
2021-06-09 00:21:48 +0900

    Add yash-semantics crate

---
+++ yash-semantics/src/lib.rs
@@ -0,0 +1,23 @@
+// This file is part of yash, an extended POSIX shell.
+// Copyright (C) 2021 WATANABE Yuki
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//! Semantics of the shell language.
+//!
+//! This crate defines the standard semantics for the shell language. The core
+//! of the semantics is word expansion and command execution. They respectively
+//! have a corresponding trait that is implemented by syntactic constructs.
+
+pub use yash_core::exec::*;
