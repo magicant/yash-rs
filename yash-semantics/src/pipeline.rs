3696549731d6539f98e3fd6d682958104b31e241
WATANABE Yuki <magicant@wonderwand.net>
2021-07-18 17:26:42 +0900

    Invert exit status when pipeline starts with !

--- yash-semantics/src/pipeline.rs
+++ yash-semantics/src/pipeline.rs
@@ -18,20 +18,52 @@
 
 use super::Command;
 use async_trait::async_trait;
+use yash_env::exec::ExitStatus;
 use yash_env::exec::Result;
 use yash_env::Env;
 use yash_syntax::syntax;
 
 #[async_trait(?Send)]
 impl Command for syntax::Pipeline {
+    /// Executes the pipeline.
+    ///
+    /// If this pipeline contains one simple command, it is executed in the
+    /// current shell execution environment.
+    ///
+    /// TODO Elaborate
+    ///
+    /// # Exit status
+    ///
+    /// If the pipeline starts with an `!`, the exit status is inverted: zero
+    /// becomes one, and non-zero becomes zero.
+    ///
+    /// In POSIX, the expected exit status is unclear when an inverted pipeline
+    /// performs a jump as in `! return 42`. The behavior disagrees among
+    /// existing shells. This implementation does not invert the exit status
+    /// when the return value is `Err(Divert::...)`, which is different from
+    /// yash 2.
     async fn execute(&self, env: &mut Env) -> Result {
-        // TODO correctly execute pipeline
-        self.commands
-            .get(0)
-            .expect("empty pipeline not yet handled")
-            .execute(env)
-            .await
+        if !self.negation {
+            return execute_commands_in_pipeline(env, &self.commands).await;
+        }
+
+        execute_commands_in_pipeline(env, &self.commands).await?;
+        env.exit_status = if env.exit_status == ExitStatus::SUCCESS {
+            ExitStatus::FAILURE
+        } else {
+            ExitStatus::SUCCESS
+        };
+        Ok(())
     }
+}
+
+async fn execute_commands_in_pipeline(env: &mut Env, commands: &[syntax::Command]) -> Result {
+    // TODO support multi-command pipelines
+    commands
+        .get(0)
+        .expect("empty pipeline not yet handled")
+        .execute(env)
+        .await
 }
 
 #[cfg(test)]
@@ -60,5 +92,35 @@ mod tests {
         let result = block_on(pipeline.execute(&mut env));
         assert_eq!(result, Err(Divert::Return));
         assert_eq!(env.exit_status, ExitStatus(37));
+    }
+
+    #[test]
+    fn inverting_exit_status_to_0_without_divert() {
+        let mut env = Env::new_virtual();
+        env.builtins.insert("return", return_builtin());
+        let pipeline: syntax::Pipeline = "! return -n 42".parse().unwrap();
+        let result = block_on(pipeline.execute(&mut env));
+        assert_eq!(result, Ok(()));
+        assert_eq!(env.exit_status, ExitStatus(0));
+    }
+
+    #[test]
+    fn inverting_exit_status_to_1_without_divert() {
+        let mut env = Env::new_virtual();
+        env.builtins.insert("return", return_builtin());
+        let pipeline: syntax::Pipeline = "! return -n 0".parse().unwrap();
+        let result = block_on(pipeline.execute(&mut env));
+        assert_eq!(result, Ok(()));
+        assert_eq!(env.exit_status, ExitStatus(1));
+    }
+
+    #[test]
+    fn not_inverting_exit_status_with_divert() {
+        let mut env = Env::new_virtual();
+        env.builtins.insert("return", return_builtin());
+        let pipeline: syntax::Pipeline = "! return 15".parse().unwrap();
+        let result = block_on(pipeline.execute(&mut env));
+        assert_eq!(result, Err(Divert::Return));
+        assert_eq!(env.exit_status, ExitStatus(15));
     }
 }
