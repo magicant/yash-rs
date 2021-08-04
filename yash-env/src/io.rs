32e857b8888edfbec298b150d72fc263290d246c
WATANABE Yuki <magicant@wonderwand.net>
2021-08-04 22:19:20 +0900

    Define AsyncIo

--- yash-env/src/io.rs
+++ yash-env/src/io.rs
@@ -16,4 +16,161 @@
 
 //! Type definitions for I/O.
 
+use nix::sys::select::FdSet;
+use std::task::Waker;
 pub use yash_syntax::syntax::Fd;
+
+/// Helper for `select`ing on FDs.
+///
+/// An `AsyncIo` is a set of [Waker]s that are waiting for an FD to be ready for
+/// reading or writing.
+///
+/// TODO Elaborate
+#[derive(Clone, Debug, Default)]
+pub struct AsyncIo {
+    readers: Vec<Awaiter>,
+    writers: Vec<Awaiter>,
+}
+
+#[derive(Clone, Debug)]
+struct Awaiter {
+    fd: Fd,
+    waker: Waker,
+}
+
+impl AsyncIo {
+    /// Returns a new empty `AsyncIo`.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Returns a set of FDs waiting for reading.
+    ///
+    /// The return value should be passed to the `select` or `pselect` system
+    /// call.
+    pub fn readers(&self) -> FdSet {
+        let mut set = FdSet::new();
+        for reader in &self.readers {
+            set.insert(reader.fd.0);
+        }
+        set
+    }
+
+    /// Returns a set of FDs waiting for writing.
+    ///
+    /// The return value should be passed to the `select` or `pselect` system
+    /// call.
+    pub fn writers(&self) -> FdSet {
+        let mut set = FdSet::new();
+        for writer in &self.writers {
+            set.insert(writer.fd.0);
+        }
+        set
+    }
+
+    /// Wakes awaiters that are ready for reading/writing.
+    ///
+    /// FDs in `readers` and `writers` are considered ready and corresponding
+    /// awaiters are woken. Once woken, awaiters are removed from `self`.
+    pub fn wake(&mut self, mut readers: FdSet, mut writers: FdSet) {
+        for i in (0..self.readers.len()).rev() {
+            if readers.contains(self.readers[i].fd.0) {
+                self.readers.remove(i).waker.wake();
+            }
+        }
+        for i in (0..self.writers.len()).rev() {
+            if writers.contains(self.writers[i].fd.0) {
+                self.writers.remove(i).waker.wake();
+            }
+        }
+    }
+
+    /// Adds an awaiter for reading.
+    pub fn wait_for_reading(&mut self, fd: Fd, waker: Waker) {
+        self.readers.push(Awaiter { fd, waker });
+    }
+
+    /// Adds an awaiter for writing.
+    pub fn wait_for_writing(&mut self, fd: Fd, waker: Waker) {
+        self.writers.push(Awaiter { fd, waker });
+    }
+
+    /// Removes all awaiters for the FD.
+    ///
+    /// The removed awaiters are woken.
+    pub fn stop_waiting(&mut self, fd: Fd) {
+        // TODO self.readers.drain_filter(|awaiter| awaiter.fd == fd);
+        // TODO self.writers.drain_filter(|awaiter| awaiter.fd == fd);
+        for i in (0..self.readers.len()).rev() {
+            if self.readers[i].fd == fd {
+                self.readers.remove(i).waker.wake();
+            }
+        }
+        for i in (0..self.writers.len()).rev() {
+            if self.writers[i].fd == fd {
+                self.writers.remove(i).waker.wake();
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::task::noop_waker;
+
+    #[test]
+    fn async_io_has_no_default_readers_or_writers() {
+        let async_io = AsyncIo::new();
+        assert_eq!(async_io.readers(), FdSet::new());
+        assert_eq!(async_io.writers(), FdSet::new());
+    }
+
+    #[test]
+    fn async_io_non_empty_readers_and_writers() {
+        let mut async_io = AsyncIo::new();
+        async_io.wait_for_reading(Fd::STDIN, noop_waker());
+        async_io.wait_for_writing(Fd::STDOUT, noop_waker());
+        async_io.wait_for_writing(Fd::STDERR, noop_waker());
+
+        let mut expected_readers = FdSet::new();
+        expected_readers.insert(Fd::STDIN.0);
+        let mut expected_writers = FdSet::new();
+        expected_writers.insert(Fd::STDOUT.0);
+        expected_writers.insert(Fd::STDERR.0);
+        assert_eq!(async_io.readers(), expected_readers);
+        assert_eq!(async_io.writers(), expected_writers);
+    }
+
+    #[test]
+    fn async_io_stop_waiting() {
+        let mut async_io = AsyncIo::new();
+        async_io.wait_for_reading(Fd::STDIN, noop_waker());
+        async_io.wait_for_writing(Fd::STDOUT, noop_waker());
+        async_io.wait_for_writing(Fd::STDERR, noop_waker());
+        async_io.stop_waiting(Fd::STDIN);
+        async_io.stop_waiting(Fd::STDERR);
+
+        assert_eq!(async_io.readers(), FdSet::new());
+
+        let mut expected_writers = FdSet::new();
+        expected_writers.insert(Fd::STDOUT.0);
+        assert_eq!(async_io.writers(), expected_writers);
+    }
+
+    #[test]
+    fn async_io_wake() {
+        let mut async_io = AsyncIo::new();
+        async_io.wait_for_reading(Fd(3), noop_waker());
+        async_io.wait_for_reading(Fd(4), noop_waker());
+        async_io.wait_for_writing(Fd(4), noop_waker());
+        let mut fds = FdSet::new();
+        fds.insert(4);
+        async_io.wake(fds, fds);
+
+        let mut expected_readers = FdSet::new();
+        expected_readers.insert(3);
+        assert_eq!(async_io.readers(), expected_readers);
+        assert_eq!(async_io.writers(), FdSet::new());
+    }
+}
