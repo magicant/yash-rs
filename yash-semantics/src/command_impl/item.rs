5fb5960b4c9a94574f2bae68a489c9ae4cff64eb
WATANABE Yuki <magicant@wonderwand.net>
2021-10-07 23:23:40 +0900

    Basic semantics of asynchronous command

--- yash-semantics/src/command_impl/item.rs
+++ yash-semantics/src/command_impl/item.rs
@@ -17,27 +17,69 @@
 //! Implementation for Item.
 
 use super::Command;
+use crate::print_error;
 use async_trait::async_trait;
+use std::ops::ControlFlow::Continue;
+use std::rc::Rc;
+use yash_env::exec::ExitStatus;
 use yash_env::exec::Result;
 use yash_env::Env;
+use yash_syntax::source::Location;
 use yash_syntax::syntax;
+use yash_syntax::syntax::AndOrList;
 
 #[async_trait(?Send)]
 impl Command for syntax::Item {
     async fn execute(&self, env: &mut Env) -> Result {
-        self.and_or.execute(env).await
-        // TODO async
+        match self.is_async {
+            false => self.and_or.execute(env).await,
+            true => execute_async(env, &self.and_or).await,
+        }
+    }
+}
+
+async fn execute_async(env: &mut Env, and_or: &Rc<AndOrList>) -> Result {
+    let and_or = Rc::clone(and_or);
+    let result = env
+        .start_subshell(|env| {
+            Box::pin(async move {
+                let _result = and_or.execute(env).await;
+                // TODO handle result
+            })
+        })
+        .await;
+    match result {
+        Ok(_pid) => {
+            env.exit_status = ExitStatus::SUCCESS;
+            Continue(())
+        }
+        Err(errno) => {
+            print_error(
+                env,
+                "cannot start a subshell to run an asynchronous command".into(),
+                errno.desc().into(),
+                &Location::dummy("X"),
+            )
+            .await;
+            // TODO Print the correct location
+
+            env.exit_status = ExitStatus::NOEXEC;
+            // TODO abort rather than continuing to the next command
+            Continue(())
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::tests::echo_builtin;
     use crate::tests::return_builtin;
+    use crate::tests::LocalExecutor;
     use futures_executor::block_on;
-    use std::ops::ControlFlow::Continue;
+    use futures_util::task::LocalSpawnExt;
     use std::rc::Rc;
-    use yash_env::exec::ExitStatus;
+    use yash_env::VirtualSystem;
 
     #[test]
     fn item_execute_sync() {
@@ -51,5 +93,80 @@ mod tests {
         let result = block_on(item.execute(&mut env));
         assert_eq!(result, Continue(()));
         assert_eq!(env.exit_status, ExitStatus(42));
+    }
+
+    #[test]
+    fn item_execute_async_exit_status() {
+        let system = VirtualSystem::new();
+        let state = Rc::clone(&system.state);
+        let mut executor = futures_executor::LocalPool::new();
+        state.borrow_mut().executor = Some(Rc::new(LocalExecutor(executor.spawner())));
+        let mut env = Env::with_system(Box::new(system));
+        env.builtins.insert("return", return_builtin());
+        env.exit_status = ExitStatus::FAILURE;
+
+        let and_or: syntax::AndOrList = "return -n 42".parse().unwrap();
+        let item = syntax::Item {
+            and_or: Rc::new(and_or),
+            is_async: true,
+        };
+        let result = executor.run_until(item.execute(&mut env));
+        assert_eq!(result, Continue(()));
+        assert_eq!(env.exit_status, ExitStatus::SUCCESS);
+    }
+
+    #[test]
+    fn item_execute_async_effect() {
+        let system = VirtualSystem::new();
+        let state = Rc::clone(&system.state);
+        let mut executor = futures_executor::LocalPool::new();
+        state.borrow_mut().executor = Some(Rc::new(LocalExecutor(executor.spawner())));
+        let mut env = Env::with_system(Box::new(system));
+        env.builtins.insert("echo", echo_builtin());
+
+        let and_or: syntax::AndOrList = "echo foo".parse().unwrap();
+        let item = syntax::Item {
+            and_or: Rc::new(and_or),
+            is_async: true,
+        };
+
+        executor
+            .spawner()
+            .spawn_local(async move {
+                let result = item.execute(&mut env).await;
+                assert_eq!(result, Continue(()));
+            })
+            .unwrap();
+        executor.run_until_stalled();
+
+        let state = state.borrow();
+        let stdout = state.file_system.get("/dev/stdout").unwrap().borrow();
+        assert_eq!(stdout.content, "foo\n".as_bytes());
+    }
+
+    #[test]
+    fn item_execute_async_fail() {
+        let system = VirtualSystem::new();
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(Box::new(system));
+        env.builtins.insert("return", return_builtin());
+
+        let and_or: syntax::AndOrList = "return -n 42".parse().unwrap();
+        let item = syntax::Item {
+            and_or: Rc::new(and_or),
+            is_async: true,
+        };
+        let result = block_on(item.execute(&mut env));
+        assert_eq!(result, Continue(()));
+        assert_eq!(env.exit_status, ExitStatus::NOEXEC);
+
+        let state = state.borrow();
+        let stderr = state.file_system.get("/dev/stderr").unwrap().borrow();
+        let stderr = std::str::from_utf8(&stderr.content).unwrap();
+        assert!(
+            stderr.contains("asynchronous"),
+            "unexpected error message: {:?}",
+            stderr
+        );
     }
 }
