5e415e34452ed0509c9529a7bc131e7b6c7b5cd8
WATANABE Yuki <magicant@wonderwand.net>
2022-03-31 01:26:24 +0900

    Remove old quote removal implementation

--- yash-semantics/src/expansion/quote_removal.rs
+++ yash-semantics/src/expansion/quote_removal.rs
@@ -47,44 +47,6 @@
 //! ```
 
 use super::AttrChar;
-use super::AttrField;
-use super::Field;
-
-/// Quote removal.
-///
-/// The quote removal is a step of the word expansion that removes quotes from
-/// the field. The [`do_quote_removal`](Self::do_quote_removal) function
-/// converts an [`AttrChar`] string to a regular string.
-pub trait QuoteRemoval {
-    /// Return type of [`do_quote_removal`](Self::do_quote_removal).
-    type Output;
-
-    /// Performs the quote removal on `self`.
-    ///
-    /// TODO Add a parameter to specify how characters in the result should be
-    /// escaped.
-    fn do_quote_removal(self) -> Self::Output;
-}
-
-impl QuoteRemoval for &[AttrChar] {
-    type Output = String;
-    fn do_quote_removal(self) -> String {
-        // TODO Remove quotes correctly
-        self.iter()
-            .filter(|c| !c.is_quoting)
-            .map(|c| c.value)
-            .collect()
-    }
-}
-
-impl QuoteRemoval for AttrField {
-    type Output = Field;
-    fn do_quote_removal(self) -> Field {
-        let AttrField { chars, origin } = self;
-        let value = chars.do_quote_removal();
-        Field { value, origin }
-    }
-}
 
 /// Performs quote removal on an iterator.
 ///
@@ -108,37 +70,6 @@ pub fn remove_quotes(chars: &mut Vec<AttrChar>) {
 mod tests {
     use super::super::Origin;
     use super::*;
-
-    #[test]
-    fn quote_removal() {
-        let a = AttrChar {
-            value: 'a',
-            origin: Origin::Literal,
-            is_quoted: false,
-            is_quoting: false,
-        };
-        let b = AttrChar {
-            value: 'b',
-            origin: Origin::Literal,
-            is_quoted: true,
-            is_quoting: false,
-        };
-        let c = AttrChar {
-            value: 'c',
-            origin: Origin::Literal,
-            is_quoted: false,
-            is_quoting: true,
-        };
-        let d = AttrChar {
-            value: 'd',
-            origin: Origin::Literal,
-            is_quoted: true,
-            is_quoting: true,
-        };
-        let input = [a, b, c, d];
-        let result = input.do_quote_removal();
-        assert_eq!(result, "ab");
-    }
 
     #[test]
     fn test_skip_quotes() {
