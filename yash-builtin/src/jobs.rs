ca1699e4d653af3990475d13a6668c14db886824
WATANABE Yuki <magicant@wonderwand.net>
2022-05-07 13:34:50 +0900

    Use yash_env::job::fmt in jobs built-in

--- yash-builtin/src/jobs.rs
+++ yash-builtin/src/jobs.rs
@@ -18,18 +18,26 @@
 //!
 //! TODO Elaborate
 
+use crate::common::arg::parse_arguments;
+use crate::common::arg::Mode;
+use crate::common::print_error_message;
 use crate::common::Print;
 use std::fmt::Write;
 use std::future::Future;
 use std::ops::ControlFlow::Continue;
 use std::pin::Pin;
 use yash_env::builtin::Result;
-use yash_env::job::WaitStatus;
+use yash_env::job::WaitStatusEx;
 use yash_env::semantics::Field;
 use yash_env::Env;
 
 /// Implementation of the jobs built-in.
-pub async fn builtin_body(env: &mut Env, _args: Vec<Field>) -> Result {
+pub async fn builtin_body(env: &mut Env, args: Vec<Field>) -> Result {
+    let (_options, _operands) = match parse_arguments(&[], Mode::default(), args) {
+        Ok(result) => result,
+        Err(error) => return print_error_message(env, &error).await,
+    };
+
     // Print jobs.
     let mut print = String::new();
     let current_job_index = env.jobs.current_job();
@@ -37,30 +45,24 @@ pub async fn builtin_body(env: &mut Env, _args: Vec<Field>) -> Result {
 
     env.jobs.drain_filter(|index, mut job| {
         // Add a line of report
-        let number = index + 1;
-        let mark = if Some(index) == current_job_index {
-            '+'
-        } else if Some(index) == previous_job_index {
-            '-'
-        } else {
-            ' '
+        use yash_env::job::fmt::{Marker, Report};
+        let report = Report {
+            index,
+            marker: if current_job_index == Some(index) {
+                Marker::CurrentJob
+            } else if previous_job_index == Some(index) {
+                Marker::PreviousJob
+            } else {
+                Marker::None
+            },
+            job: &job,
         };
-        let status = match job.status {
-            WaitStatus::Stopped(_, signal) => signal.as_str(),
-            WaitStatus::Exited(_, _) => "Done",
-            WaitStatus::Signaled(_, signal, _) => signal.as_str(),
-            _ => "Running",
-        };
-        let name = &job.name;
-        writeln!(print, "[{number}] {mark} {status:10} {name}").unwrap();
+        writeln!(print, "{}", report).unwrap();
 
         job.status_reported();
 
         // Remove terminated jobs
-        !matches!(
-            job.status,
-            WaitStatus::StillAlive | WaitStatus::Continued(_)
-        )
+        job.status.is_finished()
     });
 
     (env.print(&print).await, Continue(()))
@@ -69,4 +71,106 @@ pub async fn builtin_body(env: &mut Env, _args: Vec<Field>) -> Result {
 /// Wrapper of [`builtin_body`] that returns the future in a pinned box.
 pub fn builtin_main(env: &mut Env, args: Vec<Field>) -> Pin<Box<dyn Future<Output = Result> + '_>> {
     Box::pin(builtin_body(env, args))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_matches::assert_matches;
+    use futures_util::future::FutureExt;
+    use std::rc::Rc;
+    use std::str::from_utf8;
+    use yash_env::job::Job;
+    use yash_env::job::Pid;
+    use yash_env::job::WaitStatus;
+    use yash_env::semantics::ExitStatus;
+    use yash_env::system::r#virtual::VirtualSystem;
+    use yash_env::trap::Signal;
+
+    #[test]
+    fn no_operands_no_jobs() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let result = builtin_body(&mut env, vec![]).now_or_never().unwrap();
+        assert_eq!(result, (ExitStatus::SUCCESS, Continue(())));
+
+        let state = state.borrow();
+        let stdout = state.file_system.get("/dev/stdout").unwrap().borrow();
+        assert_eq!(from_utf8(&stdout.content), Ok(""));
+    }
+
+    #[test]
+    fn no_operands_some_jobs() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut job = Job::new(Pid::from_raw(42));
+        job.name = "echo first".to_string();
+        env.jobs.add(job);
+        let mut job = Job::new(Pid::from_raw(72));
+        job.status = WaitStatus::Stopped(Pid::from_raw(72), Signal::SIGSTOP);
+        job.name = "echo second".to_string();
+        env.jobs.add(job);
+
+        let result = builtin_body(&mut env, vec![]).now_or_never().unwrap();
+        assert_eq!(result, (ExitStatus::SUCCESS, Continue(())));
+
+        let state = state.borrow();
+        let stdout = state.file_system.get("/dev/stdout").unwrap().borrow();
+        assert_eq!(
+            from_utf8(&stdout.content),
+            Ok("[1] - Running              echo first\n[2] + Stopped(SIGSTOP)     echo second\n")
+        );
+    }
+
+    #[test]
+    fn finished_jobs_are_removed_with_no_operands() {
+        let mut env = Env::new_virtual();
+
+        let mut job = Job::new(Pid::from_raw(11));
+        job.name = "echo running".to_string();
+        let i11 = env.jobs.add(job);
+
+        let mut job = Job::new(Pid::from_raw(12));
+        job.status = WaitStatus::Stopped(Pid::from_raw(12), Signal::SIGTSTP);
+        job.name = "echo stopped".to_string();
+        let i12 = env.jobs.add(job);
+
+        let mut job = Job::new(Pid::from_raw(13));
+        job.status = WaitStatus::Continued(Pid::from_raw(13));
+        job.name = "echo continued".to_string();
+        let i13 = env.jobs.add(job);
+
+        let mut job = Job::new(Pid::from_raw(14));
+        job.status = WaitStatus::Exited(Pid::from_raw(14), 42);
+        job.name = "echo exited".to_string();
+        let i14 = env.jobs.add(job);
+
+        let mut job = Job::new(Pid::from_raw(15));
+        job.status = WaitStatus::Signaled(Pid::from_raw(15), Signal::SIGINT, false);
+        job.name = "echo signaled".to_string();
+        let i15 = env.jobs.add(job);
+
+        let mut job = Job::new(Pid::from_raw(16));
+        job.status = WaitStatus::Signaled(Pid::from_raw(16), Signal::SIGQUIT, true);
+        job.name = "echo core dumped".to_string();
+        let i16 = env.jobs.add(job);
+
+        let result = builtin_body(&mut env, vec![]).now_or_never().unwrap();
+        assert_eq!(result, (ExitStatus::SUCCESS, Continue(())));
+
+        assert_matches!(env.jobs.get(i11), Some(_));
+        assert_matches!(env.jobs.get(i12), Some(_));
+        assert_matches!(env.jobs.get(i13), Some(_));
+        assert_matches!(env.jobs.get(i14), None);
+        assert_matches!(env.jobs.get(i15), None);
+        assert_matches!(env.jobs.get(i16), None);
+    }
+
+    // TODO specifying job IDs
+    // TODO finished jobs are removed with job ID
+    // TODO specifying job IDs without the initial %
+    // TODO specifying job IDs of non-existing jobs
+    // TODO specifying ambiguous job ID
 }
--- yash-env/src/job.rs
+++ yash-env/src/job.rs
@@ -50,6 +50,18 @@ use std::collections::HashMap;
 use std::iter::FusedIterator;
 use std::ops::Deref;
 
+/// Trait for adding some methods to [`WaitStatus`]
+pub trait WaitStatusEx {
+    /// Returns true if the status indicates that the process is finished.
+    fn is_finished(&self) -> bool;
+}
+
+impl WaitStatusEx for WaitStatus {
+    fn is_finished(&self) -> bool {
+        matches!(*self, WaitStatus::Exited(..) | WaitStatus::Signaled(..))
+    }
+}
+
 /// Set of one or more processes executing a pipeline
 ///
 /// In the current implementation, a job contains the process ID of one child
