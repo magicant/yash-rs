b0e42adc16a0908f4a4e055e74dc8cd57f677a8f
WATANABE Yuki <magicant@wonderwand.net>
2022-07-10 22:47:17 +0900

    Add yash-arith crate

--- check-extra.sh
+++ check-extra.sh
@@ -8,6 +8,7 @@ cargo check
 
 cargo tomlfmt --dryrun --path Cargo.toml
 cargo tomlfmt --dryrun --path yash/Cargo.toml
+cargo tomlfmt --dryrun --path yash-arith/Cargo.toml
 cargo tomlfmt --dryrun --path yash-builtin/Cargo.toml
 cargo tomlfmt --dryrun --path yash-env/Cargo.toml
 cargo tomlfmt --dryrun --path yash-fnmatch/Cargo.toml
@@ -15,6 +16,7 @@ cargo tomlfmt --dryrun --path yash-quote/Cargo.toml
 cargo tomlfmt --dryrun --path yash-semantics/Cargo.toml
 cargo tomlfmt --dryrun --path yash-syntax/Cargo.toml
 
+cargo test --package 'yash-arith' -- $quiet
 cargo test --package 'yash-builtin' -- $quiet
 cargo test --package 'yash-env' -- $quiet
 cargo test --package 'yash-fnmatch' -- $quiet
