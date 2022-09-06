d42a757be4b7f8616d0f540e608e4280de35dcd0
WATANABE Yuki <magicant@wonderwand.net>
2022-09-07 00:39:02 +0900

    Implement break built-in

--- yash-builtin/src/break.rs
+++ yash-builtin/src/break.rs
@@ -71,15 +71,87 @@
 //! as the second element of the returned tuple. The caller must pass the value
 //! to enclosing loops so that the target loop can handle it.
 
+use crate::common::arg::parse_arguments;
+use crate::common::arg::Mode;
+use crate::common::print_error_message;
+use crate::common::BuiltinName;
 use std::future::Future;
+use std::num::ParseIntError;
+use std::ops::ControlFlow::Break;
 use std::pin::Pin;
 use yash_env::builtin::Result;
+use yash_env::semantics::Divert;
+use yash_env::semantics::ExitStatus;
 use yash_env::semantics::Field;
 use yash_env::Env;
+use yash_syntax::source::pretty::Annotation;
+use yash_syntax::source::pretty::AnnotationType;
+use yash_syntax::source::pretty::Message;
+use yash_syntax::source::Location;
+
+async fn handle_error(env: &mut Env, title: &str, annotation: Annotation<'_>) -> Result {
+    let message = Message {
+        r#type: AnnotationType::Error,
+        title: title.into(),
+        annotations: vec![annotation],
+    };
+    print_error_message(env, message).await;
+    (ExitStatus::ERROR, Break(Divert::Interrupt(None)))
+}
+
+async fn syntax_error(env: &mut Env, label: &str, location: &Location) -> Result {
+    handle_error(
+        env,
+        "command argument syntax error",
+        Annotation::new(AnnotationType::Error, label.into(), location),
+    )
+    .await
+}
+
+async fn operand_parse_error(env: &mut Env, location: &Location, error: ParseIntError) -> Result {
+    syntax_error(env, &error.to_string(), location).await
+}
+
+async fn not_in_loop_error(env: &mut Env) -> Result {
+    let location = env.stack.builtin_name().origin.clone();
+    handle_error(
+        env,
+        "cannot break",
+        Annotation::new(AnnotationType::Error, "not in loop".into(), &location),
+    )
+    .await
+}
 
 /// Implementation of the break built-in.
-pub async fn builtin_body(_env: &mut Env, _args: Vec<Field>) -> Result {
-    todo!()
+pub async fn builtin_body(env: &mut Env, args: Vec<Field>) -> Result {
+    // TODO: POSIX does not require the break built-in to support XBD Utility
+    // Syntax Guidelines. That means the built-in does not have to recognize the
+    // "--" separator. We should reject the separator in the POSIXly-correct mode.
+    let (_options, operands) = match parse_arguments(&[], Mode::with_env(env), args) {
+        Ok(result) => result,
+        Err(error) => return print_error_message(env, &error).await,
+    };
+    if let Some(operand) = operands.get(1) {
+        return syntax_error(env, "too many operands", &operand.origin).await;
+    }
+
+    let max_count = if let Some(operand) = operands.first() {
+        match operand.value.parse() {
+            Ok(0) => return syntax_error(env, "not a positive integer", &operand.origin).await,
+            Ok(max_count) => max_count,
+            Err(error) => return operand_parse_error(env, &operand.origin, error).await,
+        }
+    } else {
+        1
+    };
+
+    let count = env.stack.loop_count(max_count);
+    if count == 0 {
+        not_in_loop_error(env).await
+    } else {
+        let count = count - 1;
+        (ExitStatus::SUCCESS, Break(Divert::Break { count }))
+    }
 }
 
 /// Wrapper of [`builtin_body`] that returns the future in a pinned box.
@@ -89,13 +161,210 @@ pub fn builtin_main(env: &mut Env, args: Vec<Field>) -> Pin<Box<dyn Future<Outpu
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
+    use crate::tests::assert_stderr;
+    use crate::tests::assert_stdout;
+    use futures_util::FutureExt;
+    use std::rc::Rc;
+    use yash_env::stack::Frame;
+    use yash_env::VirtualSystem;
 
-    // TODO no_enclosing_loop
-    // TODO omitted_operand_with_one_enclosing_loop
-    // TODO omitted_operand_with_many_enclosing_loops
-    // TODO explicit_operand_1_with_many_enclosing_loops
-    // TODO explicit_operand_3_with_many_enclosing_loops
-    // TODO explicit_operand_greater_than_number_of_loops
-    // TODO malformed_operands
+    #[test]
+    fn no_enclosing_loop() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut env = env.push_frame(Frame::Builtin {
+            name: Field::dummy("break"),
+        });
+
+        let result = builtin_body(&mut env, vec![]).now_or_never().unwrap();
+        assert_eq!(result, (ExitStatus::ERROR, Break(Divert::Interrupt(None))));
+        assert_stdout(&state, |stdout| assert_eq!(stdout, ""));
+        assert_stderr(&state, |stderr| {
+            assert!(stderr.contains("not in loop"), "{:?}", stderr)
+        });
+    }
+
+    #[test]
+    fn omitted_operand_with_one_enclosing_loop() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Builtin {
+            name: Field::dummy("break"),
+        });
+
+        let result = builtin_body(&mut env, vec![]).now_or_never().unwrap();
+        assert_eq!(
+            result,
+            (ExitStatus::SUCCESS, Break(Divert::Break { count: 0 }))
+        );
+        assert_stdout(&state, |stdout| assert_eq!(stdout, ""));
+        assert_stderr(&state, |stderr| assert_eq!(stderr, ""));
+    }
+
+    #[test]
+    fn omitted_operand_with_many_enclosing_loops() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Builtin {
+            name: Field::dummy("break"),
+        });
+
+        let result = builtin_body(&mut env, vec![]).now_or_never().unwrap();
+        assert_eq!(
+            result,
+            (ExitStatus::SUCCESS, Break(Divert::Break { count: 0 }))
+        );
+        assert_stdout(&state, |stdout| assert_eq!(stdout, ""));
+        assert_stderr(&state, |stderr| assert_eq!(stderr, ""));
+    }
+
+    #[test]
+    fn explicit_operand_1_with_many_enclosing_loops() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Builtin {
+            name: Field::dummy("break"),
+        });
+        let args = Field::dummies(["1"]);
+
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
+        assert_eq!(
+            result,
+            (ExitStatus::SUCCESS, Break(Divert::Break { count: 0 }))
+        );
+        assert_stdout(&state, |stdout| assert_eq!(stdout, ""));
+        assert_stderr(&state, |stderr| assert_eq!(stderr, ""));
+    }
+
+    #[test]
+    fn explicit_operand_3_with_many_enclosing_loops() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Builtin {
+            name: Field::dummy("break"),
+        });
+        let args = Field::dummies(["3"]);
+
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
+        assert_eq!(
+            result,
+            (ExitStatus::SUCCESS, Break(Divert::Break { count: 2 }))
+        );
+        assert_stdout(&state, |stdout| assert_eq!(stdout, ""));
+        assert_stderr(&state, |stderr| assert_eq!(stderr, ""));
+    }
+
+    #[test]
+    fn explicit_operand_greater_than_number_of_loops() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Builtin {
+            name: Field::dummy("break"),
+        });
+        let args = Field::dummies(["5"]);
+
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
+        assert_eq!(
+            result,
+            (ExitStatus::SUCCESS, Break(Divert::Break { count: 3 }))
+        );
+        assert_stdout(&state, |stdout| assert_eq!(stdout, ""));
+        assert_stderr(&state, |stderr| assert_eq!(stderr, ""));
+    }
+
+    #[test]
+    fn explicit_operand_0() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Builtin {
+            name: Field::dummy("break"),
+        });
+        let args = Field::dummies(["0"]);
+
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
+        assert_eq!(result, (ExitStatus::ERROR, Break(Divert::Interrupt(None))));
+        assert_stdout(&state, |stdout| assert_eq!(stdout, ""));
+        assert_stderr(&state, |stderr| {
+            assert!(stderr.contains("not a positive integer"), "{:?}", stderr)
+        });
+    }
+
+    #[test]
+    fn explicit_operand_too_large() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Builtin {
+            name: Field::dummy("break"),
+        });
+        let args = Field::dummies(["999999999999999999999999999999"]);
+
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
+        assert_eq!(result, (ExitStatus::ERROR, Break(Divert::Interrupt(None))));
+        assert_stdout(&state, |stdout| assert_eq!(stdout, ""));
+        assert_stderr(&state, |stderr| assert_ne!(stderr, ""));
+    }
+
+    #[test]
+    fn too_many_operands() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Builtin {
+            name: Field::dummy("break"),
+        });
+        let args = Field::dummies(["1", "1"]);
+
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
+        assert_eq!(result, (ExitStatus::ERROR, Break(Divert::Interrupt(None))));
+        assert_stdout(&state, |stdout| assert_eq!(stdout, ""));
+        assert_stderr(&state, |stderr| {
+            assert!(stderr.contains("too many operands"), "{:?}", stderr)
+        });
+    }
+
+    #[test]
+    fn argument_separator() {
+        let system = Box::new(VirtualSystem::new());
+        let state = Rc::clone(&system.state);
+        let mut env = Env::with_system(system);
+        let mut env = env.push_frame(Frame::Loop);
+        let mut env = env.push_frame(Frame::Builtin {
+            name: Field::dummy("break"),
+        });
+        let args = Field::dummies(["--", "1"]);
+
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
+        assert_eq!(
+            result,
+            (ExitStatus::SUCCESS, Break(Divert::Break { count: 0 }))
+        );
+        assert_stdout(&state, |stdout| assert_eq!(stdout, ""));
+        assert_stderr(&state, |stderr| assert_eq!(stderr, ""));
+    }
 }
