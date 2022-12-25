f09749e41a9025f5cb7f2f86e2c37a519ca256f6
WATANABE Yuki <magicant@wonderwand.net>
2022-12-25 12:08:33 +0900

    Extract open_fd function
    Refactoring...

--- yash-semantics/src/redir/here_doc.rs
+++ yash-semantics/src/redir/here_doc.rs
@@ -29,10 +29,30 @@ use yash_env::System;
 use yash_syntax::source::Location;
 use yash_syntax::syntax::HereDoc;
 
-async fn write_content(env: &mut Env, fd: Fd, content: &str) -> Result<(), Errno> {
+async fn fill_content(env: &mut Env, fd: Fd, content: &str) -> Result<(), Errno> {
     env.system.write_all(fd, content.as_bytes()).await?;
     env.system.lseek(fd, std::io::SeekFrom::Start(0))?;
     Ok(())
+}
+
+/// Opens a here-document.
+///
+/// This function writes the here-document content to an anonymous temporary
+/// file and returns a file descriptor to the file you can read the content
+/// from.
+pub(super) async fn open_fd(env: &mut Env, content: String) -> Result<Fd, ErrorCause> {
+    // TODO Use a pipe for short content
+    let fd = match env.system.open_tmpfile(Path::new("/tmp")) {
+        Ok(fd) => fd,
+        Err(errno) => return Err(ErrorCause::TemporaryFileUnavailable(errno)),
+    };
+    match fill_content(env, fd, &content).await {
+        Ok(()) => Ok(fd),
+        Err(errno) => {
+            let _ = env.system.close(fd);
+            Err(ErrorCause::TemporaryFileUnavailable(errno))
+        }
+    }
 }
 
 /// Opens a here-document.
@@ -47,31 +67,10 @@ pub(super) async fn open(
 ) -> Result<(FdSpec, Location, Option<ExitStatus>), Error> {
     let (content, exit_status) = expand_text(env, &here_doc.content.borrow()).await?;
     let location = here_doc.delimiter.location.clone();
-
-    // TODO Use a pipe for short content
-
-    let fd = match env.system.open_tmpfile(Path::new("/tmp")) {
-        Ok(fd) => fd,
-        Err(errno) => {
-            return Err(Error {
-                cause: ErrorCause::TemporaryFileUnavailable(errno),
-                location,
-            })
-        }
-    };
-
-    match write_content(env, fd, &content).await {
-        Ok(()) => (),
-        Err(errno) => {
-            let _ = env.system.close(fd);
-            return Err(Error {
-                cause: ErrorCause::TemporaryFileUnavailable(errno),
-                location,
-            });
-        }
-    };
-
-    Ok((FdSpec::Owned(fd), location, exit_status))
+    match open_fd(env, content).await {
+        Ok(fd) => Ok((FdSpec::Owned(fd), location, exit_status)),
+        Err(cause) => Err(Error { cause, location }),
+    }
 }
 
 #[cfg(test)]
@@ -85,6 +84,20 @@ mod tests {
     use std::cell::RefCell;
     use yash_env::System;
     use yash_syntax::syntax::Text;
+
+    #[test]
+    fn open_fd_and_read_from_it() {
+        let text = "Here document content\n";
+        let mut env = Env::new_virtual();
+        let fd = open_fd(&mut env, text.to_owned())
+            .now_or_never()
+            .unwrap()
+            .unwrap();
+
+        let mut buffer = [0; 30];
+        let count = env.system.read(fd, &mut buffer).unwrap();
+        assert_eq!(std::str::from_utf8(&buffer[..count]), Ok(text));
+    }
 
     #[test]
     fn empty_here_doc() {
