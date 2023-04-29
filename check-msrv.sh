141b00415148f5ce44a9f2db88ddb5ade650bcf5
WATANABE Yuki <magicant@wonderwand.net>
2023-04-30 00:21:18 +0900

    Precise MSRV check
    In the check-msrv.sh script, we now check the MSRV for each crate with
    non-dependent crates removed from the workspace. This ensures the crate
    is tested only with its own dependencies.

--- check-msrv.sh
+++ check-msrv.sh
@@ -4,18 +4,47 @@ if [ "$*" = "" ]; then quiet='--quiet'; else quiet=''; fi
 
 set -x
 
-# Make sure Cargo.lock is clean before running cargo update
-git diff --exit-code -- Cargo.lock
-trap 'git checkout -- Cargo.lock' EXIT
+# Make sure the files are clean before we modify them
+git diff --exit-code -- Cargo.lock Cargo.toml
+trap 'git checkout -- Cargo.lock Cargo.toml' EXIT
 
+update_workspace_member() {
+    cat >| Cargo.toml <<EOF
+[workspace]
+members = ["$1"]
+resolver = "2"
+EOF
+}
+
+update_workspace_member yash-arith
 cargo +nightly update -Z direct-minimal-versions
+cargo +1.58.0 test --package yash-arith -- $quiet
 
-cargo +1.60.0 test --package 'yash-arith' -- $quiet
-cargo +1.67.0 test --package 'yash-builtin' -- $quiet
-cargo +1.67.0 test --package 'yash-env' -- $quiet
-cargo +1.60.0 test --package 'yash-fnmatch' -- $quiet
-cargo +1.60.0 test --package 'yash-quote' -- $quiet
-cargo +1.67.0 test --package 'yash-semantics' -- $quiet
-cargo +1.67.0 test --package 'yash-syntax' -- $quiet
-cargo +1.67.0 test --package 'yash-syntax' --features annotate-snippets -- $quiet
-cargo +1.67.0 test --package 'yash' -- $quiet
+update_workspace_member yash-builtin
+cargo +nightly update -Z direct-minimal-versions
+cargo +1.67.0 test --package yash-builtin -- $quiet
+
+update_workspace_member yash-env
+cargo +nightly update -Z direct-minimal-versions
+cargo +1.67.0 test --package yash-env -- $quiet
+
+update_workspace_member yash-fnmatch
+cargo +nightly update -Z direct-minimal-versions
+cargo +1.60.0 test --package yash-fnmatch -- $quiet
+
+update_workspace_member yash-quote
+cargo +nightly update -Z direct-minimal-versions
+cargo +1.56.0 test --package yash-quote -- $quiet
+
+update_workspace_member yash-semantics
+cargo +nightly update -Z direct-minimal-versions
+cargo +1.67.0 test --package yash-semantics -- $quiet
+
+update_workspace_member yash-syntax
+cargo +nightly update -Z direct-minimal-versions
+cargo +1.67.0 test --package yash-syntax -- $quiet
+cargo +1.67.0 test --package yash-syntax --features annotate-snippets -- $quiet
+
+update_workspace_member yash
+cargo +nightly update -Z direct-minimal-versions
+cargo +1.67.0 test --package yash -- $quiet
