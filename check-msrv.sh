b6eea1a6a90463b37c07a15a4380a7045a8100e1
WATANABE Yuki <magicant@wonderwand.net>
2023-04-29 16:28:17 +0900

    Check with newer version of cargo
    It seems cargo versions earlier than 1.60.0 have a bug in resolving
    dependencies specified in the current Cargo.lock. As a workaround, we
    now check with 1.60.0.

--- check-msrv.sh
+++ check-msrv.sh
@@ -8,13 +8,13 @@ set -x
 git diff --exit-code -- Cargo.lock
 trap 'git checkout -- Cargo.lock' EXIT
 
-cargo +nightly update -Z minimal-versions
+cargo +nightly update -Z direct-minimal-versions
 
-cargo +1.58.0 test --package 'yash-arith' -- $quiet
+cargo +1.60.0 test --package 'yash-arith' -- $quiet
 cargo +1.67.0 test --package 'yash-builtin' -- $quiet
 cargo +1.67.0 test --package 'yash-env' -- $quiet
-cargo +1.58.0 test --package 'yash-fnmatch' -- $quiet
-cargo +1.56.0 test --package 'yash-quote' -- $quiet
+cargo +1.60.0 test --package 'yash-fnmatch' -- $quiet
+cargo +1.60.0 test --package 'yash-quote' -- $quiet
 cargo +1.67.0 test --package 'yash-semantics' -- $quiet
 cargo +1.67.0 test --package 'yash-syntax' -- $quiet
 cargo +1.67.0 test --package 'yash-syntax' --features annotate-snippets -- $quiet
