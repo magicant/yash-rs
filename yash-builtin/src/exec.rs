b61d12147a274c0a7f67b5ffa2c66d296071d691
WATANABE Yuki <magicant@wonderwand.net>
2023-05-26 00:16:43 +0900

    exec: Error handling

--- yash-builtin/src/exec.rs
+++ yash-builtin/src/exec.rs
@@ -89,12 +89,15 @@
 
 use std::ffi::CString;
 use std::future::Future;
+use std::ops::ControlFlow::Break;
 use std::pin::Pin;
 use yash_env::builtin::Result;
 use yash_env::semantics::Field;
 use yash_env::Env;
 use yash_semantics::command::simple_command::{replace_current_process, to_c_strings};
 use yash_semantics::command_search::search_path;
+use yash_semantics::Divert::Exit;
+use yash_semantics::ExitStatus;
 
 /// Implements the exec built-in.
 pub async fn builtin_body(env: &mut Env, args: Vec<Field>) -> Result {
@@ -103,10 +106,19 @@ pub async fn builtin_body(env: &mut Env, args: Vec<Field>) -> Result {
     result.retain_redirs();
 
     if let Some(name) = args.first() {
+        // TODO Returning Exit will execute the EXIT trap before exiting, which is not expected
+        result.set_divert(Break(Exit(None)));
+
         let path = if name.value.contains('/') {
             CString::new(name.value.clone()).unwrap_or_default()
         } else {
-            search_path(env, name.value.as_str()).unwrap()
+            match search_path(env, name.value.as_str()) {
+                Some(path) => path,
+                None => {
+                    result.set_exit_status(ExitStatus::NOT_FOUND);
+                    return result;
+                }
+            }
         };
         let location = name.origin.clone();
         let args = to_c_strings(args);
@@ -131,7 +143,6 @@ mod tests {
     use yash_env::system::r#virtual::{FileBody, INode};
     use yash_env::variable::{Scope, Variable};
     use yash_env::VirtualSystem;
-    use yash_semantics::ExitStatus;
 
     #[test]
     fn retains_redirs_without_args() {
@@ -252,6 +263,60 @@ mod tests {
         assert_eq!(arguments.2, []);
     }
 
-    // TODO error in searching for the external utility
-    // TODO error in executing the external utility
+    #[test]
+    fn utility_not_found() {
+        let system = VirtualSystem::new();
+        let mut env = Env::with_system(Box::new(system.clone()));
+
+        // Prepare the external utility file
+        let mut content = INode::default();
+        content.body = FileBody::Regular {
+            content: Vec::new(),
+            is_native_executable: true,
+        };
+        content.permissions.0 |= 0o100;
+        let content = Rc::new(RefCell::new(content));
+        system
+            .state
+            .borrow_mut()
+            .file_system
+            .save("/bin/echo", content)
+            .unwrap();
+
+        // No PATH variable
+
+        let args = Field::dummies(["echo"]);
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
+        assert_eq!(result.exit_status(), ExitStatus::NOT_FOUND);
+        assert_eq!(result.divert(), Break(Exit(None)));
+
+        let process = &system.current_process();
+        assert_eq!(process.last_exec(), &None);
+    }
+
+    #[test]
+    fn utility_not_executable() {
+        let system = VirtualSystem::new();
+        let mut env = Env::with_system(Box::new(system.clone()));
+
+        // Prepare the file without executable permission
+        let mut content = INode::default();
+        content.body = FileBody::Regular {
+            content: Vec::new(),
+            is_native_executable: true,
+        };
+        // content.permissions.0 |= 0o100;
+        let content = Rc::new(RefCell::new(content));
+        system
+            .state
+            .borrow_mut()
+            .file_system
+            .save("/bin/echo", content)
+            .unwrap();
+
+        let args = Field::dummies(["/bin/echo"]);
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
+        assert_eq!(result.exit_status(), ExitStatus::NOEXEC);
+        assert_eq!(result.divert(), Break(Exit(None)));
+    }
 }
