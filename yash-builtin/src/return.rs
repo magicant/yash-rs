a23bc889ffc50fce7d1bbb45fe91fabad4f5c2a2
WATANABE Yuki <magicant@wonderwand.net>
2023-06-17 11:20:56 +0900

    Make return built-in async
    It has to be async since it may need to print an error message.

--- yash-builtin/src/return.rs
+++ yash-builtin/src/return.rs
@@ -88,7 +88,6 @@
 //! built-in is invoked in a trap executed in the function or script, the caller
 //! should use the value of `$?` before entering trap.
 
-use std::future::ready;
 use std::future::Future;
 use std::ops::ControlFlow::Break;
 use std::pin::Pin;
@@ -101,7 +100,7 @@ use yash_env::Env;
 /// Implementation of the return built-in.
 ///
 /// See the [module-level documentation](self) for details.
-pub fn builtin_main_sync(_env: &mut Env, args: Vec<Field>) -> Result {
+pub async fn builtin_body(_env: &mut Env, args: Vec<Field>) -> Result {
     // TODO Parse arguments correctly
     // TODO Reject returning from an interactive session
     let mut i = args.iter().peekable();
@@ -122,25 +121,25 @@ pub fn builtin_main_sync(_env: &mut Env, args: Vec<Field>) -> Result {
 
 /// Implementation of the return built-in.
 ///
-/// This function calls [`builtin_main_sync`] and wraps the result in a
-/// `Future`.
+/// This function calls [`builtin_body`] and wraps the result in a `Box`.
 pub fn builtin_main(
     env: &mut yash_env::Env,
     args: Vec<Field>,
-) -> Pin<Box<dyn Future<Output = Result>>> {
-    Box::pin(ready(builtin_main_sync(env, args)))
+) -> Pin<Box<dyn Future<Output = Result> + '_>> {
+    Box::pin(builtin_body(env, args))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use futures_util::FutureExt;
     use yash_env::semantics::ExitStatus;
 
     #[test]
     fn returns_exit_status_specified_without_n_option() {
         let mut env = Env::new_virtual();
         let args = Field::dummies(["42"]);
-        let actual_result = builtin_main_sync(&mut env, args);
+        let actual_result = builtin_body(&mut env, args).now_or_never().unwrap();
         let mut expected_result = Result::new(ExitStatus(42));
         expected_result.set_divert(Break(Divert::Return(None)));
         assert_eq!(actual_result, expected_result);
@@ -150,7 +149,7 @@ mod tests {
     fn returns_exit_status_12_with_n_option() {
         let mut env = Env::new_virtual();
         let args = Field::dummies(["-n", "12"]);
-        let result = builtin_main_sync(&mut env, args);
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
         assert_eq!(result, Result::new(ExitStatus(12)));
     }
 
@@ -158,7 +157,7 @@ mod tests {
     fn returns_exit_status_47_with_n_option() {
         let mut env = Env::new_virtual();
         let args = Field::dummies(["-n", "47"]);
-        let result = builtin_main_sync(&mut env, args);
+        let result = builtin_body(&mut env, args).now_or_never().unwrap();
         assert_eq!(result, Result::new(ExitStatus(47)));
     }
 }
