686ffc36d091c1ee7df1c3145d69c8a5e72df10e
WATANABE Yuki <magicant@wonderwand.net>
2023-07-17 02:14:32 +0900

    Replace break built-in test with scripted test
    This commit imports the break built-in test from the yash repository.
    https://github.com/magicant/yash/commit/6da3c0bb0d541a3ebb0bd6ce88969debed13378b
    
    The run-test.sh script has been modified from the original so that it
    can be run with bash.

--- yash/tests/builtin/mod.rs
+++ yash/tests/builtin/mod.rs
@@ -14,6 +14,5 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-mod r#break;
 mod exit;
 mod r#return;
---
+++ yash/tests/scripted_test.rs
@@ -0,0 +1,49 @@
+// This file is part of yash, an extended POSIX shell.
+// Copyright (C) 2023 WATANABE Yuki
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use std::path::Path;
+use std::process::Command;
+use std::process::Stdio;
+
+const BIN: &str = env!("CARGO_BIN_EXE_yash");
+const TMPDIR: &str = env!("CARGO_TARGET_TMPDIR");
+
+fn run(name: &str) {
+    // TODO Reset signal blocking mask
+
+    let mut log_file = Path::new(TMPDIR).join(name);
+    log_file.set_extension("log");
+
+    let result = Command::new("sh")
+        .env("TMPDIR", TMPDIR)
+        .current_dir("tests/scripted_test")
+        .stdin(Stdio::null())
+        .arg("./run-test.sh")
+        .arg(BIN)
+        .arg(name)
+        .arg(&log_file)
+        .output()
+        .unwrap();
+    assert!(result.status.success(), "{:?}", result);
+
+    let log = std::fs::read_to_string(&log_file).unwrap();
+    assert!(!log.contains("FAILED"), "{}", log);
+}
+
+#[test]
+fn break_builtin() {
+    run("break-p.sh")
+}
