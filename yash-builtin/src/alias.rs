9b5fef2ae6b0c7b293cc72208a0486a1e8f0ef95
WATANABE Yuki <magicant@wonderwand.net>
2023-12-16 17:22:36 +0900

    Document alias built-in

--- yash-builtin/src/alias.rs
+++ yash-builtin/src/alias.rs
@@ -16,7 +16,48 @@
 
 //! Alias built-in.
 //!
-//! TODO Elaborate
+//! The **`alias`** built-in defines [aliases] or prints alias definitions.
+//!
+//! [aliases]: yash_syntax::alias
+//!
+//! # Synopsis
+//!
+//! ```sh
+//! alias [name[=value]…]
+//! ```
+//!
+//! # Description
+//!
+//! The alias built-in defines aliases or prints alias definitions as specified
+//! by the operands. If there are no operands, the alias built-in prints all
+//! alias definitions. The printed definitions are in the form of assignments
+//! with proper quoting that can be used as operands to the alias built-in to
+//! restore the definitions.
+//!
+//! # Options
+//!
+//! None. (TODO: Non-POSIX options)
+//!
+//! # Operands
+//!
+//! Each operand must be of the form `name=value` or `name`. The first form
+//! defines an alias named *name* that expands to *value*. The second form
+//! prints the definition of the alias named *name*.
+//!
+//! # Errors
+//!
+//! It is an error if an operand without `=` names a non-existent alias.
+//!
+//! # Exit status
+//!
+//! Zero unless an error occurs.
+//!
+//! # Portability
+//!
+//! The alias built-in is specified in POSIX.
+//!
+//! Some shells have a set of predefined aliases that are printed even if you
+//! don't define any explicitly.
 
 use yash_env::builtin::Result;
 use yash_env::semantics::ExitStatus;
