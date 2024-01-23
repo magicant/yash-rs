cb52343d93780420b04a7995425a87b87b9d44e7
WATANABE Yuki <magicant@wonderwand.net>
2024-01-24 00:16:14 +0900

    [T]::expand

--- yash-semantics/src/expansion/initial/slice.rs
+++ yash-semantics/src/expansion/initial/slice.rs
@@ -89,6 +89,18 @@ impl<T: Expand> Expand for [T] {
         }
         Ok(phrase)
     }
+
+    async fn expand(&self, env: &mut Env<'_>) -> Result<Phrase, Error> {
+        if self.is_empty() {
+            return Ok(Phrase::one_empty_field());
+        }
+
+        let mut phrase = Phrase::zero_fields();
+        for item in self {
+            phrase += item.expand(env).await?;
+        }
+        Ok(phrase)
+    }
 }
 
 #[cfg(test)]
@@ -97,8 +109,7 @@ mod tests {
     use crate::expansion::attr::AttrChar;
     use crate::expansion::attr::Origin;
     use crate::expansion::Error;
-    use assert_matches::assert_matches;
-    use futures_util::FutureExt;
+    use futures_util::FutureExt as _;
     use std::cell::Cell;
 
     fn dummy_attr_char(value: char) -> AttrChar {
@@ -110,48 +121,19 @@ mod tests {
         }
     }
 
-    enum Stub {
-        Quick(Cell<Option<Result<Phrase, Error>>>),
-        Async(Cell<Option<Result<Phrase, Error>>>),
-    }
+    struct Stub(Cell<Option<Result<Phrase, Error>>>);
 
     impl Stub {
-        fn new_quick(result: Result<Phrase, Error>) -> Self {
-            Stub::Quick(Cell::new(Some(result)))
-        }
-        fn new_async(result: Result<Phrase, Error>) -> Self {
-            Stub::Async(Cell::new(Some(result)))
-        }
-    }
-
-    impl Debug for Stub {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            match self {
-                Stub::Quick(_) => "Quick",
-                Stub::Async(_) => "Async",
-            }
-            .fmt(f)
+        fn new(result: Result<Phrase, Error>) -> Self {
+            Stub(Cell::new(Some(result)))
         }
     }
 
     impl Expand for Stub {
         type Interim = ();
 
-        fn quick_expand(&self, _: &mut Env<'_>) -> QuickExpand<()> {
-            if let Stub::Quick(cell) = self {
-                Ready(
-                    cell.take()
-                        .expect("quick_expand should be called only once"),
-                )
-            } else {
-                Interim(())
-            }
-        }
-
-        async fn async_expand(&self, _: &mut Env<'_>, _: ()) -> Result<Phrase, Error> {
-            assert_matches!(self, Stub::Async(cell) => {
-                cell.take().expect("async_expand should be called only once")
-            })
+        async fn expand(&self, _: &mut Env<'_>) -> Result<Phrase, Error> {
+            self.0.take().expect("expand should be called only once")
         }
     }
 
@@ -160,157 +142,58 @@ mod tests {
         let mut env = yash_env::Env::new_virtual();
         let mut env = Env::new(&mut env);
         let stubs: [Stub; 0] = [];
-        assert_matches!(stubs.quick_expand(&mut env), Ready(result) => {
-            assert_eq!(result, Ok(Phrase::one_empty_field()));
-        });
+        let result = stubs.expand(&mut env).now_or_never().unwrap();
+        assert_eq!(result, Ok(Phrase::one_empty_field()));
     }
 
     #[test]
-    fn slice_of_one_quick_item_returning_zero_fields() {
+    fn slice_of_one_item_returning_zero_fields() {
         let mut env = yash_env::Env::new_virtual();
         let mut env = Env::new(&mut env);
-        let stubs = [Stub::new_quick(Ok(Phrase::zero_fields()))];
-        assert_matches!(stubs.quick_expand(&mut env), Ready(result) => {
-            assert_eq!(result, Ok(Phrase::zero_fields()));
-        });
+        let stubs = [Stub::new(Ok(Phrase::zero_fields()))];
+        let result = stubs.expand(&mut env).now_or_never().unwrap();
+        assert_eq!(result, Ok(Phrase::zero_fields()));
     }
 
     #[test]
-    fn slice_of_many_quick_items_returning_zero_fields() {
-        let mut env = yash_env::Env::new_virtual();
-        let mut env = Env::new(&mut env);
-        let stubs = [
-            Stub::new_quick(Ok(Phrase::zero_fields())),
-            Stub::new_quick(Ok(Phrase::zero_fields())),
-            Stub::new_quick(Ok(Phrase::zero_fields())),
-        ];
-        assert_matches!(stubs.quick_expand(&mut env), Ready(result) => {
-            assert_eq!(result, Ok(Phrase::zero_fields()));
-        });
-    }
-
-    #[test]
-    fn slice_of_many_quick_items_each_returning_one_empty_field() {
+    fn slice_of_many_items_returning_zero_fields() {
         let mut env = yash_env::Env::new_virtual();
         let mut env = Env::new(&mut env);
         let stubs = [
-            Stub::new_quick(Ok(Phrase::one_empty_field())),
-            Stub::new_quick(Ok(Phrase::one_empty_field())),
-            Stub::new_quick(Ok(Phrase::one_empty_field())),
+            Stub::new(Ok(Phrase::zero_fields())),
+            Stub::new(Ok(Phrase::zero_fields())),
+            Stub::new(Ok(Phrase::zero_fields())),
         ];
-        assert_matches!(stubs.quick_expand(&mut env), Ready(result) => {
-            assert_eq!(result, Ok(Phrase::one_empty_field()));
-        });
+        let result = stubs.expand(&mut env).now_or_never().unwrap();
+        assert_eq!(result, Ok(Phrase::zero_fields()));
     }
 
     #[test]
-    fn slice_of_many_quick_items_each_returning_one_non_empty_field() {
+    fn slice_of_many_items_each_returning_one_empty_field() {
+        let mut env = yash_env::Env::new_virtual();
+        let mut env = Env::new(&mut env);
+        let stubs = [
+            Stub::new(Ok(Phrase::one_empty_field())),
+            Stub::new(Ok(Phrase::one_empty_field())),
+            Stub::new(Ok(Phrase::one_empty_field())),
+        ];
+        let result = stubs.expand(&mut env).now_or_never().unwrap();
+        assert_eq!(result, Ok(Phrase::one_empty_field()));
+    }
+
+    #[test]
+    fn slice_of_many_items_each_returning_one_non_empty_field() {
         let mut env = yash_env::Env::new_virtual();
         let mut env = Env::new(&mut env);
         let a = dummy_attr_char('a');
         let b = dummy_attr_char('b');
         let c = dummy_attr_char('c');
         let stubs = [
-            Stub::new_quick(Ok(Phrase::Char(a))),
-            Stub::new_quick(Ok(Phrase::Char(b))),
-            Stub::new_quick(Ok(Phrase::Char(c))),
+            Stub::new(Ok(Phrase::Char(a))),
+            Stub::new(Ok(Phrase::Char(b))),
+            Stub::new(Ok(Phrase::Char(c))),
         ];
-        assert_matches!(stubs.quick_expand(&mut env), Ready(result) => {
-            assert_eq!(result, Ok(Phrase::Field(vec![a, b, c])));
-        });
-    }
-
-    #[test]
-    fn slice_of_one_async_item_returning_zero_fields() {
-        let mut env = yash_env::Env::new_virtual();
-        let mut env = Env::new(&mut env);
-        let stubs = [Stub::new_async(Ok(Phrase::zero_fields()))];
-        assert_matches!(stubs.quick_expand(&mut env), Interim(interim) => {
-            let result = stubs
-                .async_expand(&mut env, interim)
-                .now_or_never()
-                .unwrap();
-            assert_eq!(result, Ok(Phrase::zero_fields()));
-        });
-    }
-
-    #[test]
-    fn slice_of_many_async_items_returning_zero_fields() {
-        let mut env = yash_env::Env::new_virtual();
-        let mut env = Env::new(&mut env);
-        let stubs = [
-            Stub::new_async(Ok(Phrase::zero_fields())),
-            Stub::new_async(Ok(Phrase::zero_fields())),
-            Stub::new_async(Ok(Phrase::zero_fields())),
-        ];
-        assert_matches!(stubs.quick_expand(&mut env), Interim(interim) => {
-            let result = stubs
-                .async_expand(&mut env, interim)
-                .now_or_never()
-                .unwrap();
-            assert_eq!(result, Ok(Phrase::zero_fields()));
-        });
-    }
-
-    #[test]
-    fn slice_of_many_async_items_each_returning_one_empty_field() {
-        let mut env = yash_env::Env::new_virtual();
-        let mut env = Env::new(&mut env);
-        let stubs = [
-            Stub::new_async(Ok(Phrase::one_empty_field())),
-            Stub::new_async(Ok(Phrase::one_empty_field())),
-            Stub::new_async(Ok(Phrase::one_empty_field())),
-        ];
-        assert_matches!(stubs.quick_expand(&mut env), Interim(interim) => {
-            let result = stubs
-                .async_expand(&mut env, interim)
-                .now_or_never()
-                .unwrap();
-            assert_eq!(result, Ok(Phrase::one_empty_field()));
-        });
-    }
-
-    #[test]
-    fn slice_of_many_async_items_each_returning_one_non_empty_field() {
-        let mut env = yash_env::Env::new_virtual();
-        let mut env = Env::new(&mut env);
-        let a = dummy_attr_char('a');
-        let b = dummy_attr_char('b');
-        let c = dummy_attr_char('c');
-        let stubs = [
-            Stub::new_async(Ok(Phrase::Char(a))),
-            Stub::new_async(Ok(Phrase::Char(b))),
-            Stub::new_async(Ok(Phrase::Char(c))),
-        ];
-        assert_matches!(stubs.quick_expand(&mut env), Interim(interim) => {
-            let result = stubs
-                .async_expand(&mut env, interim)
-                .now_or_never()
-                .unwrap();
-            assert_eq!(result, Ok(Phrase::Field(vec![a, b, c])));
-        });
-    }
-
-    #[test]
-    fn slice_of_quick_and_async_items() {
-        let mut env = yash_env::Env::new_virtual();
-        let mut env = Env::new(&mut env);
-        let a = dummy_attr_char('a');
-        let b = dummy_attr_char('b');
-        let c = dummy_attr_char('c');
-        let stubs = [
-            Stub::new_quick(Ok(Phrase::Char(c))),
-            Stub::new_async(Ok(Phrase::zero_fields())),
-            Stub::new_quick(Ok(Phrase::Char(b))),
-            Stub::new_async(Ok(Phrase::Char(a))),
-            Stub::new_quick(Ok(Phrase::one_empty_field())),
-        ];
-        assert_matches!(stubs.quick_expand(&mut env), Interim(interim) => {
-            let result = stubs
-                .async_expand(&mut env, interim)
-                .now_or_never()
-                .unwrap();
-            assert_eq!(result, Ok(Phrase::Field(vec![c, b, a])));
-        });
+        let result = stubs.expand(&mut env).now_or_never().unwrap();
+        assert_eq!(result, Ok(Phrase::Field(vec![a, b, c])));
     }
 }
