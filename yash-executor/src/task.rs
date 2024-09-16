3b0dd4cbfe2f5f4428f222741359ba2ba35417b7
WATANABE Yuki <magicant@wonderwand.net>
2024-09-17 01:01:40 +0900

    More implementation of Task::poll

--- yash-executor/src/task.rs
+++ yash-executor/src/task.rs
@@ -22,21 +22,24 @@ impl Task<'_> {
     /// If `self.executor` has been dropped or the task is polled recursively,
     /// this method panics.
     pub fn poll(self: &Rc<Self>) -> bool {
-        let mut future = self.future.borrow_mut();
-        match future.as_mut() {
-            None => todo!(),
-            Some(future) => {
-                if self.executor.strong_count() == 0 {
-                    todo!("executor has been dropped");
-                }
-                let waker = futures_task::noop_waker();
-                let mut context = Context::from_waker(&waker);
-                let poll = future.as_mut().poll(&mut context);
-                true // TODO false if poll is not ready
-            }
+        let mut future_or_none = self
+            .future
+            .try_borrow_mut()
+            .expect("future polled recursively");
+        let Some(future) = future_or_none.as_mut() else {
+            return true;
+        };
+        if self.executor.strong_count() == 0 {
+            todo!("executor has been dropped");
         }
-
-        // TODO Change self.future to None if the future is complete
+        let waker = futures_task::noop_waker();
+        let mut context = Context::from_waker(&waker);
+        let poll = future.as_mut().poll(&mut context);
+        let is_ready = poll.is_ready();
+        if is_ready {
+            *future_or_none = None;
+        }
+        is_ready
     }
 }
 
@@ -46,10 +49,11 @@ mod tests {
     use alloc::boxed::Box;
     use alloc::rc::Weak;
     use core::cell::{Cell, RefCell};
+    use core::future::pending;
 
     #[test]
     #[should_panic = "executor has been dropped"]
-    fn polling_without_executor() {
+    fn polling_without_executor_panics() {
         let task = Rc::new(Task {
             executor: Weak::new(),
             future: RefCell::new(Some(Box::pin(async { unreachable!() }))),
@@ -67,5 +71,46 @@ mod tests {
         });
         assert!(task.poll());
         assert!(polled.get());
+    }
+
+    #[test]
+    fn polling_pending_future() {
+        let executor = Rc::default();
+        let task = Rc::new(Task {
+            executor: Rc::downgrade(&executor),
+            future: RefCell::new(Some(Box::pin(pending()))),
+        });
+        assert!(!task.poll());
+    }
+
+    #[test]
+    fn polling_pending_future_again_should_do_nothing() {
+        let poll_count = Rc::new(Cell::new(0));
+        let executor = Rc::default();
+        let task = Rc::new(Task {
+            executor: Rc::downgrade(&executor),
+            future: RefCell::new(Some(Box::pin(async {
+                poll_count.set(poll_count.get() + 1)
+            }))),
+        });
+        assert!(task.poll());
+        assert_eq!(poll_count.get(), 1);
+        assert!(task.poll());
+        assert_eq!(poll_count.get(), 1);
+    }
+
+    #[test]
+    #[should_panic = "future polled recursively"]
+    fn recursive_poll_panics() {
+        let executor = Rc::default();
+        let task = Rc::new(Task {
+            executor: Rc::downgrade(&executor),
+            future: RefCell::new(None),
+        });
+        let task2 = Rc::clone(&task);
+        *task.future.borrow_mut() = Some(Box::pin(async move {
+            task2.poll();
+        }));
+        task.poll();
     }
 }
