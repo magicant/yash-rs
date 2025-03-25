1dd742b60a2aa2081bf99917e3050dc2b1fba60a
WATANABE Yuki <magicant@wonderwand.net>
2025-03-26 01:01:27 +0900

    Reimport job-y.sh test file
    https://github.com/magicant/yash/blob/8732f7f55fa5c1bf082d403c7a44f630727eaf38/tests/job-y.tst

--- yash-cli/tests/scripted_test/job-y.sh
+++ yash-cli/tests/scripted_test/job-y.sh
@@ -1,12 +1,19 @@
 # job-y.sh: yash-specific test of job control
 
+user_id="$(id -u)"
+
 # This test case first creates a background job that immediately exits, then
 # waits for the job to finish, sending a null signal to the job to poll if the
 # job is still running. A subshell starts another job and waits for it to finish
 # to make sure the main shell process receives the SIGCHLD signal and examines
 # the latest job status. The test case checks if the job is reported as done
 # before the prompt for the next line is displayed.
-test_e 'interactive shell reports job status before prompt' -im
+(
+if [ "$user_id" -eq 0 ]; then
+    skip="true"
+fi
+
+test_e 'interactive shell reports job status before prompt (non-root)' -im
 echo >&2; sleep 0& while kill -0 $! 2>/dev/null; do :; done; (sleep 0& wait)
 echo done >&2; exit
 __IN__
@@ -14,6 +21,22 @@ $
 [1] + Done                 sleep 0
 $ done
 __ERR__
+)
+
+(
+if [ "$user_id" -ne 0 ]; then
+    skip="true"
+fi
+
+test_e 'interactive shell reports job status before prompt (root)' -im
+echo >&2; sleep 0& while kill -0 $! 2>/dev/null; do :; done; (sleep 0& wait)
+echo done >&2; exit
+__IN__
+# 
+[1] + Done                 sleep 0
+# done
+__ERR__
+)
 
 mkfifo sync
 
@@ -28,3 +51,12 @@ cat sync
 :
 wait $pid
 __IN__
+
+# This is a POSIX requirement, but this test case depends on the shell's
+# behavior that runs all pipeline components in child processes.
+test_o -e 0 'discard remaining commands when a command suspends' -im
+echo resumed | { kill -STOP 0; cat; }; echo not printed 1; echo not printed 2&
+fg >/dev/null
+__IN__
+resumed
+__OUT__
