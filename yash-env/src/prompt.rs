37c126ea453516dc096120ea4a4f9e988df751a1
WATANABE Yuki <magicant@wonderwand.net>
2025-10-19 00:35:29 +0900

    Inject prompt dependency into yash-builtin thru yash-env
    This is the first step to flatten the dependency graph by removing the
    direct dependency of yash-builtin on yash-prompt.
    <https://github.com/magicant/yash-rs/issues/621>
    
    The yash-builtin crate no longer depends directly on the yash-prompt
    crate. The function to obtain prompt strings is now injected into the
    crate via the environment's any storage, using the GetPrompt type
    defined in the yash-env crate.

---
+++ yash-env/src/prompt.rs
@@ -0,0 +1,64 @@
+// This file is part of yash, an extended POSIX shell.
+// Copyright (C) 2025 WATANABE Yuki
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//! Types for injecting prompt string retrieval functions
+//!
+//! This module defines the type used for dependency injection:
+//! a function that obtains the prompt string from the environment.
+//!
+//! The concrete implementation of that function is not provided by this crate;
+//! it is implemented in another crate and stored inside [`Env::any`]. Callers
+//! that need a prompt (for example the interactive input loop or the `read`
+//! built-in) retrieve the injected function from `Env::any` and invoke it to
+//! produce prompt strings.
+//!
+//! This file only supplies the type used for injection; registration and
+//! concrete implementations are the responsibility of other crates that set
+//! up the environment.
+
+use crate::Env;
+use crate::input::Context;
+use std::pin::Pin;
+
+/// Wrapper around the function that retrieves a prompt string
+///
+/// The wrapped function is expected to be injected into the environment (via
+/// [`Env::any`]) by another crate and may be referenced by multiple consumers.
+///
+/// The function takes two arguments: the environment and the input context. It
+/// returns a future that resolves to the final prompt string to be displayed.
+/// Any errors encountered while generating the prompt string should be handled
+/// within the function itself; the function should always resolve to a valid
+/// string.
+///
+/// The most standard way to implement such a function is to use the
+/// [`yash-prompt` crate](yash-prompt):
+///
+/// ```
+/// # use yash_env::prompt::GetPrompt;
+/// let mut env = yash_env::Env::new_virtual();
+/// env.any.insert(Box::new(GetPrompt(|env, context| {
+///     Box::pin(async move {
+///         let prompt = yash_prompt::fetch_posix(&env.variables, &context);
+///         yash_prompt::expand_posix(env, &prompt, false).await
+///     })
+/// })));
+/// ```
+#[derive(Clone, Copy, Debug)]
+pub struct GetPrompt(
+    #[allow(clippy::type_complexity)]
+    pub  for<'a> fn(&'a mut Env, &'a Context) -> Pin<Box<dyn Future<Output = String> + 'a>>,
+);
