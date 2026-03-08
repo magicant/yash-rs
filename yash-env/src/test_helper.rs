83ae4facaeb05f12ebb3753bd0fbd8daeec7eb36
WATANABE Yuki <magicant@wonderwand.net>
2026-03-08 16:31:17 +0900

    Typo

--- yash-env/src/test_helper.rs
+++ yash-env/src/test_helper.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-//! Utilities for performing tests that interact with the shell environment.
+//! Utilities for performing tests that interact with the shell environment
 //!
 //! This module is conditionally compiled when the `test-helper` feature is enabled.
 
