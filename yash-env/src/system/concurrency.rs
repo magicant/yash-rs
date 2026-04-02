07e138bff8cdafd0dfbaf84336e6a1de32241445
WATANABE Yuki <magicant@wonderwand.net>
2026-04-03 01:22:27 +0900

    Implement some system traits for Rc<Concurrent<_>>

--- yash-env/src/system/concurrency.rs
+++ yash-env/src/system/concurrency.rs
@@ -16,7 +16,14 @@
 
 //! Items for concurrent task execution
 
-use super::{Pipe, Read, Select, Write};
+use super::{
+    Close, Dir, Dup, Fcntl, FdFlag, Fstat, IsExecutableFile, OfdAccess, Open, Pipe, Read, Result,
+    Select, Write,
+};
+use crate::io::Fd;
+use enumset::EnumSet;
+use std::ffi::CStr;
+use std::rc::Rc;
 
 /// Decorator for systems that makes blocking I/O operations concurrency-friendly
 ///
@@ -45,5 +52,134 @@ impl<S> Concurrent<S> {
     #[must_use]
     pub fn new(inner: S) -> Self {
         Self { inner }
+    }
+}
+
+impl<S> Fstat for Rc<Concurrent<S>>
+where
+    S: Fstat,
+{
+    type Stat = S::Stat;
+
+    #[inline]
+    fn fstat(&self, fd: Fd) -> Result<Self::Stat> {
+        self.inner.fstat(fd)
+    }
+
+    #[inline]
+    fn fstatat(&self, dir_fd: Fd, path: &CStr, follow_symlinks: bool) -> Result<Self::Stat> {
+        self.inner.fstatat(dir_fd, path, follow_symlinks)
+    }
+
+    #[inline]
+    fn is_directory(&self, path: &CStr) -> bool {
+        self.inner.is_directory(path)
+    }
+
+    #[inline]
+    fn fd_is_pipe(&self, fd: Fd) -> bool {
+        self.inner.fd_is_pipe(fd)
+    }
+}
+
+impl<S> IsExecutableFile for Rc<Concurrent<S>>
+where
+    S: IsExecutableFile,
+{
+    #[inline]
+    fn is_executable_file(&self, path: &CStr) -> bool {
+        self.inner.is_executable_file(path)
+    }
+}
+
+impl<S> Pipe for Rc<Concurrent<S>>
+where
+    S: Pipe,
+{
+    #[inline]
+    fn pipe(&self) -> Result<(Fd, Fd)> {
+        self.inner.pipe()
+    }
+}
+
+impl<S> Dup for Rc<Concurrent<S>>
+where
+    S: Dup,
+{
+    #[inline]
+    fn dup(&self, from: Fd, to_min: Fd, flags: EnumSet<FdFlag>) -> Result<Fd> {
+        self.inner.dup(from, to_min, flags)
+    }
+
+    #[inline]
+    fn dup2(&self, from: Fd, to: Fd) -> Result<Fd> {
+        self.inner.dup2(from, to)
+    }
+}
+
+/// This implementation does not (yet) support non-blocking open operations.
+impl<S> Open for Rc<Concurrent<S>>
+where
+    S: Open,
+{
+    #[inline]
+    fn open(
+        &self,
+        path: &CStr,
+        access: OfdAccess,
+        flags: EnumSet<super::OpenFlag>,
+        mode: super::Mode,
+    ) -> impl Future<Output = Result<Fd>> + use<S> {
+        self.inner.open(path, access, flags, mode)
+    }
+
+    #[inline]
+    fn open_tmpfile(&self, parent_dir: &unix_path::Path) -> Result<Fd> {
+        self.inner.open_tmpfile(parent_dir)
+    }
+
+    #[inline]
+    fn fdopendir(&self, fd: Fd) -> Result<impl Dir + use<S>> {
+        self.inner.fdopendir(fd)
+    }
+
+    #[inline]
+    fn opendir(&self, path: &CStr) -> Result<impl Dir + use<S>> {
+        self.inner.opendir(path)
+    }
+}
+
+impl<S> Close for Rc<Concurrent<S>>
+where
+    S: Close,
+{
+    #[inline]
+    fn close(&self, fd: Fd) -> Result<()> {
+        self.inner.close(fd)
+    }
+}
+
+impl<S> Fcntl for Rc<Concurrent<S>>
+where
+    S: Fcntl,
+{
+    #[inline]
+    fn ofd_access(&self, fd: Fd) -> Result<OfdAccess> {
+        self.inner.ofd_access(fd)
+    }
+
+    #[inline]
+    fn get_and_set_nonblocking(&self, fd: Fd, nonblocking: bool) -> Result<bool> {
+        self.inner.get_and_set_nonblocking(fd, nonblocking)
+    }
+
+    #[inline]
+    fn fcntl_getfd(&self, fd: Fd) -> Result<EnumSet<FdFlag>> {
+        self.inner.fcntl_getfd(fd)
+    }
+
+    #[inline]
+    fn fcntl_setfd(&self, fd: Fd, flags: EnumSet<FdFlag>) -> Result<()> {
+        self.inner.fcntl_setfd(fd, flags)
     }
 }
