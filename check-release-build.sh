ef07f5a0039ccd1d16de2a1edb82b512b335614f
WATANABE Yuki <magicant@wonderwand.net>
2026-04-30 13:17:51 +0900

    Revise feature combination in release build checks
    Checking for the yash-executor feature of the yash-env crate was missing,
    which is fixed in this commit.
    
    The checks for the yash-builtin and yash-env crates with default
    features enabled are also removed, since they are already covered by the
    other checks with no default features (and selected features) enabled.

--- check-release-build.sh
+++ check-release-build.sh
@@ -5,13 +5,12 @@ set -Ceux
 # doesn't break due to any missing dependencies or similar issues.
 # Other check scripts only perform debug builds, so they won't catch such issues.
 cargo build --release --package 'yash-arith' --all-targets
-cargo build --release --package 'yash-builtin' --all-targets
 cargo build --release --package 'yash-builtin' --all-targets --no-default-features
 cargo build --release --package 'yash-builtin' --all-targets --no-default-features --features yash-semantics
 cargo build --release --package 'yash-cli' --all-targets
-cargo build --release --package 'yash-env' --all-targets
 cargo build --release --package 'yash-env' --all-targets --no-default-features
 cargo build --release --package 'yash-env' --all-targets --no-default-features --features test-helper
+cargo build --release --package 'yash-env' --all-targets --no-default-features --features yash-executor
 cargo build --release --package 'yash-env-test-helper' --all-targets
 cargo build --release --package 'yash-executor' --all-targets
 cargo build --release --package 'yash-fnmatch' --all-targets
