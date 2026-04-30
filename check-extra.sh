027f401d31a11b5b205e2ecca501a1061da54b65
WATANABE Yuki <magicant@wonderwand.net>
2026-04-30 11:49:53 +0900

    Test yash-executor feature of yash-env crate
    Commit ec273b93 added the yash-executor feature to the yash-env crate,
    but it was not tested in check-extra.sh. This commit adds checks for
    the feature that are done for the other existing features.

--- check-extra.sh
+++ check-extra.sh
@@ -14,6 +14,9 @@ taplo lint $(git ls-files | grep '\.toml$')
 
 # Make sure we don't have any unnecessary dependencies in Cargo.toml.
 RUSTFLAGS='-D unused_crate_dependencies' cargo check --lib --all-features
+RUSTFLAGS='-D unused_crate_dependencies' cargo check --package 'yash-env' --no-default-features
+RUSTFLAGS='-D unused_crate_dependencies' cargo check --package 'yash-env' --no-default-features --features test-helper
+RUSTFLAGS='-D unused_crate_dependencies' cargo check --package 'yash-env' --no-default-features --features yash-executor
 RUSTFLAGS='-D unused_crate_dependencies' cargo check --package 'yash-builtin' --no-default-features
 RUSTFLAGS='-D unused_crate_dependencies' cargo check --package 'yash-builtin' --no-default-features --features yash-semantics
 
@@ -41,6 +44,7 @@ cargo test --package 'yash-builtin' --features yash-semantics -- $quiet
 #cargo test --package 'yash-cli' -- $quiet
 #cargo test --package 'yash-env' -- $quiet
 cargo test --package 'yash-env' --features test-helper -- $quiet
+cargo test --package 'yash-env' --features yash-executor -- $quiet
 #cargo test --package 'yash-fnmatch' -- $quiet
 #cargo test --package 'yash-prompt' -- $quiet
 #cargo test --package 'yash-quote' -- $quiet
