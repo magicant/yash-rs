magicant/yash-rs

commit 2d388ff457d494fa18229758bc91ec5b504da9c6

Require Sigmask bound for Concurrent
This commit adds a requirement for the inner system type `S` of
`yash_env::system::Concurrent<S>` to implement the
`yash_env::system::Sigmask` trait. This prepares the `Concurrent` struct
for managing signal masks using the `Sigset` type from the `Sigmask`
trait.

yash-env/src/system/concurrency.rs
@@ -20,7 +20,9 @@
 //! systems that enables concurrent execution of multiple possibly blocking I/O
 //! tasks on a single thread.
 
-use super::{CaughtSignals, ChildProcessStarter, Clock, Errno, Fcntl, Fork, Read, Result, Write};
+use super::{
+    CaughtSignals, ChildProcessStarter, Clock, Errno, Fcntl, Fork, Read, Result, Sigmask, Write,
+};
 use crate::io::Fd;
 use crate::job::Pid;
 use crate::waker::{ScheduledWakerQueue, WakerSet};
@@ -103,7 +105,7 @@ use std::time::{Duration, Instant};
 ///
 /// [`Pipe`]: super::Pipe
 #[derive(Clone, Debug, Default)]
-pub struct Concurrent<S> {
+pub struct Concurrent<S: Sigmask> {
     inner: S,
     state: RefCell<State>,
 }
@@ -162,7 +164,7 @@ impl State {
     }
 }
 
-impl<S> Concurrent<S> {
+impl<S: Sigmask> Concurrent<S> {
     /// Creates a new `Concurrent` system that wraps the given inner system.
     #[must_use]
     pub fn new(inner: S) -> Self {
@@ -182,7 +184,7 @@ impl<S> Concurrent<S> {
 /// reading.
 impl<S> Read for Rc<Concurrent<S>>
 where
-    S: Fcntl + Read,
+    S: Fcntl + Read + Sigmask,
 {
     fn read<'a>(
         &self,
@@ -221,7 +223,7 @@ where
 /// writing.
 impl<S> Write for Rc<Concurrent<S>>
 where
-    S: Fcntl + Write,
+    S: Fcntl + Write + Sigmask,
 {
     fn write<'a>(
         &self,
@@ -249,7 +251,7 @@ where
     }
 }
 
-impl<S> Concurrent<S> {
+impl<S: Sigmask> Concurrent<S> {
     async fn yield_for_read<F>(&self, fd: Fd, waker: &LazyCell<Rc<Cell<Option<Waker>>>, F>)
     where
         F: FnOnce() -> Rc<Cell<Option<Waker>>>,
@@ -310,7 +312,7 @@ pub trait Sleep {
 
 impl<S> Sleep for Rc<S>
 where
-    S: Sleep,
+    S: Sigmask + Sleep,
 {
     #[inline]
     fn sleep_until(&self, deadline: Instant) -> impl Future<Output = ()> {
@@ -325,7 +327,7 @@ where
 
 impl<S> Sleep for Concurrent<S>
 where
-    S: Clock,
+    S: Clock + Sigmask,
 {
     async fn sleep_until(&self, deadline: Instant) {
         let waker: LazyCell<Rc<Cell<Option<Waker>>>> = LazyCell::default();
@@ -381,7 +383,7 @@ where
     }
 }
 
-impl<S> WaitForSignals for Concurrent<S> {
+impl<S: Sigmask> WaitForSignals for Concurrent<S> {
     async fn wait_for_signals(&self) -> Rc<SignalList> {
         let signals = {
             let mut state = self.state.borrow_mut();
@@ -475,7 +477,7 @@ where
 
 impl<S> Select for Concurrent<S>
 where
-    S: CaughtSignals + Clock + super::Select,
+    S: CaughtSignals + Clock + super::Select + Sigmask,
 {
     fn peek(&self) {
         let select = pin!(self.select_impl(true));
@@ -490,7 +492,7 @@ where
 
 impl<S> Concurrent<S>
 where
-    S: CaughtSignals + Clock + super::Select,
+    S: CaughtSignals + Clock + super::Select + Sigmask,
 {
     #[allow(
         clippy::await_holding_refcell_ref,
@@ -565,13 +567,13 @@ fn wake_tasks_for_ready_fds(task_map: &mut HashMap<Fd, WakerSet>, ready_fds: &[F
 /// Guard for temporarily setting a file descriptor to non-blocking mode and
 /// restoring the original blocking mode when dropped
 #[derive(Debug)]
-struct TemporaryNonBlockingGuard<'a, S: Fcntl> {
+struct TemporaryNonBlockingGuard<'a, S: Fcntl + Sigmask> {
     system: &'a Concurrent<S>,
     fd: Fd,
     original_nonblocking: bool,
 }
 
-impl<'a, S: Fcntl> TemporaryNonBlockingGuard<'a, S> {
+impl<'a, S: Fcntl + Sigmask> TemporaryNonBlockingGuard<'a, S> {
     fn new(system: &'a Concurrent<S>, fd: Fd) -> Self {
         Self {
             system,
@@ -581,7 +583,7 @@ impl<'a, S: Fcntl> TemporaryNonBlockingGuard<'a, S> {
     }
 }
 
-impl<'a, S: Fcntl> Drop for TemporaryNonBlockingGuard<'a, S> {
+impl<'a, S: Fcntl + Sigmask> Drop for TemporaryNonBlockingGuard<'a, S> {
     fn drop(&mut self) {
         if !self.original_nonblocking {
             self.system
@@ -592,7 +594,7 @@ impl<'a, S: Fcntl> Drop for TemporaryNonBlockingGuard<'a, S> {
     }
 }
 
-impl<'a, S: Fcntl> Deref for TemporaryNonBlockingGuard<'a, S> {
+impl<'a, S: Fcntl + Sigmask> Deref for TemporaryNonBlockingGuard<'a, S> {
     type Target = Concurrent<S>;
 
     fn deref(&self) -> &Self::Target {
@@ -641,7 +643,7 @@ impl SignalList {
 
 impl<S> Fork for Rc<Concurrent<S>>
 where
-    S: Fork + RunLoop + Sized,
+    S: Fork + RunLoop + Sigmask + Sized,
 {
     /// Runs a task in a new child process.
     ///
@@ -710,7 +712,7 @@ pub trait RunLoop {
         task: F,
     ) -> impl Future<Output = ()> + use<'c, Self, F>
     where
-        Self: Sized,
+        Self: Sigmask + Sized,
         F: Future<Output = ()>;
 }
 

yash-env/src/system/concurrency/delegates.rs
@@ -39,7 +39,7 @@ use unix_str::UnixString;
 
 impl<S> Fstat for Concurrent<S>
 where
-    S: Fstat,
+    S: Fstat + Sigmask,
 {
     type Stat = S::Stat;
 
@@ -63,7 +63,7 @@ where
 
 impl<S> IsExecutableFile for Concurrent<S>
 where
-    S: IsExecutableFile,
+    S: IsExecutableFile + Sigmask,
 {
     #[inline]
     fn is_executable_file(&self, path: &CStr) -> bool {
@@ -73,7 +73,7 @@ where
 
 impl<S> Pipe for Concurrent<S>
 where
-    S: Pipe,
+    S: Pipe + Sigmask,
 {
     #[inline]
     fn pipe(&self) -> Result<(Fd, Fd)> {
@@ -83,7 +83,7 @@ where
 
 impl<S> Dup for Concurrent<S>
 where
-    S: Dup,
+    S: Dup + Sigmask,
 {
     #[inline]
     fn dup(&self, from: Fd, to_min: Fd, flags: EnumSet<FdFlag>) -> Result<Fd> {
@@ -99,7 +99,7 @@ where
 /// This implementation does not (yet) support non-blocking open operations.
 impl<S> Open for Concurrent<S>
 where
-    S: Open,
+    S: Open + Sigmask,
 {
     #[inline]
     fn open(
@@ -130,7 +130,7 @@ where
 
 impl<S> Close for Concurrent<S>
 where
-    S: Close,
+    S: Close + Sigmask,
 {
     #[inline]
     fn close(&self, fd: Fd) -> Result<()> {
@@ -140,7 +140,7 @@ where
 
 impl<S> Fcntl for Concurrent<S>
 where
-    S: Fcntl,
+    S: Fcntl + Sigmask,
 {
     #[inline]
     fn ofd_access(&self, fd: Fd) -> Result<OfdAccess> {
@@ -165,7 +165,7 @@ where
 
 impl<S> Seek for Concurrent<S>
 where
-    S: Seek,
+    S: Seek + Sigmask,
 {
     #[inline]
     fn lseek(&self, fd: Fd, position: SeekFrom) -> Result<u64> {
@@ -175,7 +175,7 @@ where
 
 impl<S> Umask for Concurrent<S>
 where
-    S: Umask,
+    S: Sigmask + Umask,
 {
     #[inline]
     fn umask(&self, new_mask: Mode) -> Mode {
@@ -185,7 +185,7 @@ where
 
 impl<S> GetCwd for Concurrent<S>
 where
-    S: GetCwd,
+    S: GetCwd + Sigmask,
 {
     #[inline]
     fn getcwd(&self) -> Result<PathBuf> {
@@ -195,7 +195,7 @@ where
 
 impl<S> Chdir for Concurrent<S>
 where
-    S: Chdir,
+    S: Chdir + Sigmask,
 {
     #[inline]
     fn chdir(&self, path: &CStr) -> Result<()> {
@@ -205,7 +205,7 @@ where
 
 impl<S> Clock for Concurrent<S>
 where
-    S: Clock,
+    S: Clock + Sigmask,
 {
     #[inline]
     fn now(&self) -> Instant {
@@ -215,7 +215,7 @@ where
 
 impl<S> Times for Concurrent<S>
 where
-    S: Times,
+    S: Sigmask + Times,
 {
     #[inline]
     fn times(&self) -> Result<CpuTimes> {
@@ -225,7 +225,7 @@ where
 
 impl<S> GetPid for Concurrent<S>
 where
-    S: GetPid,
+    S: GetPid + Sigmask,
 {
     #[inline]
     fn getpid(&self) -> Pid {
@@ -247,7 +247,7 @@ where
 
 impl<S> SetPgid for Concurrent<S>
 where
-    S: SetPgid,
+    S: SetPgid + Sigmask,
 {
     #[inline]
     fn setpgid(&self, pid: Pid, pgid: Pid) -> Result<()> {
@@ -257,7 +257,7 @@ where
 
 impl<S> Signals for Concurrent<S>
 where
-    S: Signals,
+    S: Sigmask,
 {
     const SIGABRT: signal::Number = S::SIGABRT;
     const SIGALRM: signal::Number = S::SIGALRM;
@@ -368,7 +368,7 @@ where
 
 impl<S> GetSigaction for Concurrent<S>
 where
-    S: GetSigaction,
+    S: GetSigaction + Sigmask,
 {
     #[inline]
     fn get_sigaction(&self, signal: signal::Number) -> Result<signal::Disposition> {
@@ -397,7 +397,7 @@ where
 /// [`SignalSystem`]: crate::trap::SignalSystem
 impl<S> Sigaction for Concurrent<S>
 where
-    S: Sigaction,
+    S: Sigaction + Sigmask,
 {
     #[inline]
     fn sigaction(
@@ -425,7 +425,7 @@ where
 
 impl<S> SendSignal for Concurrent<S>
 where
-    S: SendSignal,
+    S: SendSignal + Sigmask,
 {
     #[inline]
     fn kill(
@@ -443,7 +443,7 @@ where
 
 impl<S> Isatty for Concurrent<S>
 where
-    S: Isatty,
+    S: Isatty + Sigmask,
 {
     #[inline]
     fn isatty(&self, fd: Fd) -> bool {
@@ -453,7 +453,7 @@ where
 
 impl<S> TcGetPgrp for Concurrent<S>
 where
-    S: TcGetPgrp,
+    S: Sigmask + TcGetPgrp,
 {
     #[inline]
     fn tcgetpgrp(&self, fd: Fd) -> Result<Pid> {
@@ -463,7 +463,7 @@ where
 
 impl<S> TcSetPgrp for Concurrent<S>
 where
-    S: TcSetPgrp,
+    S: Sigmask + TcSetPgrp,
 {
     #[inline]
     fn tcsetpgrp(&self, fd: Fd, pgid: Pid) -> impl Future<Output = Result<()>> + use<S> {
@@ -473,7 +473,7 @@ where
 
 impl<S> Concurrent<S>
 where
-    S: Fork,
+    S: Fork + Sigmask,
 {
     /// Creates a new child process.
     ///
@@ -494,7 +494,7 @@ where
 
 impl<S> Wait for Concurrent<S>
 where
-    S: Wait,
+    S: Sigmask + Wait,
 {
     #[inline]
     fn wait(&self, target: Pid) -> Result<Option<(Pid, ProcessState)>> {
@@ -504,7 +504,7 @@ where
 
 impl<S> Exec for Concurrent<S>
 where
-    S: Exec,
+    S: Exec + Sigmask,
 {
     #[inline]
     fn execve(
@@ -519,7 +519,7 @@ where
 
 impl<S> Exit for Concurrent<S>
 where
-    S: Exit,
+    S: Exit + Sigmask,
 {
     #[inline]
     fn exit(&self, exit_status: ExitStatus) -> impl Future<Output = Infallible> + use<S> {
@@ -529,7 +529,7 @@ where
 
 impl<S> GetUid for Concurrent<S>
 where
-    S: GetUid,
+    S: GetUid + Sigmask,
 {
     #[inline]
     fn getuid(&self) -> Uid {
@@ -551,7 +551,7 @@ where
 
 impl<S> GetPw for Concurrent<S>
 where
-    S: GetPw,
+    S: GetPw + Sigmask,
 {
     #[inline]
     fn getpwnam_dir(&self, name: &CStr) -> Result<Option<PathBuf>> {
@@ -561,7 +561,7 @@ where
 
 impl<S> Sysconf for Concurrent<S>
 where
-    S: Sysconf,
+    S: Sysconf + Sigmask,
 {
     #[inline]
     fn confstr_path(&self) -> Result<UnixString> {
@@ -571,7 +571,7 @@ where
 
 impl<S> ShellPath for Concurrent<S>
 where
-    S: ShellPath,
+    S: ShellPath + Sigmask,
 {
     #[inline]
     fn shell_path(&self) -> CString {
@@ -581,7 +581,7 @@ where
 
 impl<S> GetRlimit for Concurrent<S>
 where
-    S: GetRlimit,
+    S: GetRlimit + Sigmask,
 {
     #[inline]
     fn getrlimit(&self, resource: Resource) -> Result<LimitPair> {
@@ -591,7 +591,7 @@ where
 
 impl<S> SetRlimit for Concurrent<S>
 where
-    S: SetRlimit,
+    S: SetRlimit + Sigmask,
 {
     #[inline]
     fn setrlimit(&self, resource: Resource, limits: LimitPair) -> Result<()> {

yash-env/src/system/concurrency/rw_all.rs
@@ -18,7 +18,7 @@
 
 use super::{Concurrent, TemporaryNonBlockingGuard};
 use crate::io::Fd;
-use crate::system::{Errno, Fcntl, Read, Write};
+use crate::system::{Errno, Fcntl, Read, Sigmask, Write};
 use std::cell::LazyCell;
 use std::iter::repeat_n;
 use std::rc::Rc;
@@ -66,7 +66,7 @@ where
 
 impl<S> ReadAll for Concurrent<S>
 where
-    S: Fcntl + Read,
+    S: Fcntl + Read + Sigmask,
 {
     async fn read_all_to(&self, fd: Fd, buffer: &mut Vec<u8>) -> Result<(), Errno> {
         let this = TemporaryNonBlockingGuard::new(self, fd);
@@ -140,7 +140,7 @@ where
 
 impl<S> WriteAll for Concurrent<S>
 where
-    S: Fcntl + Write,
+    S: Fcntl + Sigmask + Write,
 {
     async fn write_all(&self, fd: Fd, mut data: &[u8]) -> Result<(), Errno> {
         if data.is_empty() {

yash-env/src/system/process.rs
@@ -16,7 +16,7 @@
 
 //! Items related to process management
 
-use super::{Concurrent, ExitStatus, Result, Signals};
+use super::{Concurrent, ExitStatus, Result, Sigmask, Signals};
 use crate::Env;
 #[cfg(all(doc, unix))]
 use crate::RealSystem;
@@ -197,7 +197,7 @@ pub trait Fork {
     )]
     fn new_child_process(&self) -> Result<ChildProcessStarter<Self>>
     where
-        Self: Sized;
+        Self: Sigmask + Sized;
 }
 
 /// Trait for waiting for child processes

commit 02fbee8de2f19b75fe0eeed6ac8d914909ca63e2

Don't require Sigmask for Rc<S> to implement Sleep
The commit 2d388ff4 mistakenly added a requirement for Sigmask to be
implemented for Rc<S> to implement Sleep, which is unnecessary. This
commit reverts that change.

yash-env/src/system/concurrency.rs
@@ -313,7 +313,7 @@ pub trait Sleep {
 
 impl<S> Sleep for Rc<S>
 where
-    S: Sigmask + Sleep,
+    S: Sleep,
 {
     #[inline]
     fn sleep_until(&self, deadline: Instant) -> impl Future<Output = ()> {